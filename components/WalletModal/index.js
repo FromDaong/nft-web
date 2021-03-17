ec54a66bf90e002b6707288452786355891c86e4
Luca <lucamarchal@gmail.com>
2021-03-17 02:52:04 +0000

    modal update

---
+++ components/WalletModal/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Model from "react-bootstrap/Model";
+
+// import blur from "/assets/blur.png";
+// import "./index.scss";
+
+const NFTListItem = ({ data }) => {
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default NFTListItem;
