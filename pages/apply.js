542cf4f44a4636ca3015fead64518bb98e9da070
Luca <lucamarchal@gmail.com>
2021-06-19 17:01:04 +0100

    typeform

--- pages/apply.js
+++ pages/apply.js
@@ -4,13 +4,7 @@ import { Widget } from "@typeform/embed-react";
 const Home = () => {
   return (
     <>
-      <h1
-        style={{ textAlign: "center", fontWeight: "bolder" }}
-        className="p-5 m-5 heading-text"
-      >
-        Coming very soon...
-      </h1>
-      {/* <Widget id="u79qd4FI" style={{ height: "75vh" }} className="mt-4" /> */}
+      <Widget id="qSBmDwLO" style={{ height: "75vh" }} className="mt-4" />
     </>
   );
 };
