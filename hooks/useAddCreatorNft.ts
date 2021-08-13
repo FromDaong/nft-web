9ea14a899e30a32bb848284d2ccf79364f243ef2
s-m-o-l <71986235+s-m-o-l@users.noreply.github.com>
2021-08-13 16:17:42 -0700

    Update useAddCreatorNft.ts

--- hooks/useAddCreatorNft.ts
+++ hooks/useAddCreatorNft.ts
@@ -1,25 +1,23 @@
-import { getCreatorMartContract, addCreatorNFTs } from "../treat/utils";
+import { getCreatorMartContract, addCreatorNft } from "../treat/utils";
 import { useCallback } from "react";
 import useTreat from "./useTreat";
 import { useWallet } from "use-wallet";
 
-const useAddCreatorNFTs = (ids: Array<number>, amounts: Array<number>, hexData: string) => {
+const useAddCreatorNFTs = (ids: Array<number>, amounts: Array<number>) => {
   const { account } = useWallet();
   const treat = useTreat();
   const creatorMartContract = getCreatorMartContract(treat);
 
   const handleAddCreatorNFTs = useCallback(async () => {
-    const txHash = await addCreatorNFTs(
+    const txHash = await addCreatorNft(
       creatorMartContract,
       account,
-      maxSupplys,
-      amounts,
-      isGiveAwayFlags,
-      hexData
+      ids,
+      amounts
     );
     console.log(txHash);
     return txHash;
-  }, [account, maxSupplys, amounts, isGiveAwayFlags, hexData, creatorMartContract]);
+  }, [account, ids, amounts, creatorMartContract]);
 
   return { onAddCreatorNFTs: handleAddCreatorNFTs };
 };
