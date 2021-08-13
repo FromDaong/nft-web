ea8ea03218143f717c37da7644ce46aca5f5f200
s-m-o-l <71986235+s-m-o-l@users.noreply.github.com>
2021-08-13 15:56:33 -0700

    Hooks For Creator Performer Check

---
+++ hooks/useGetIsPerformer.ts
@@ -0,0 +1,23 @@
+import { isPerformerForMinter, getTreatNFTMinterContract } from "../treat/utils";
+import { useCallback } from "react";
+import useTreat from "./useTreat";
+import { useWallet } from "use-wallet";
+
+const useGetIsPerformer = (modelAddress: string) => {
+  const { account } = useWallet();
+  const treat = useTreat();
+  const treatNftMinterContract = getTreatNFTMinterContract(treat);
+
+  const handleGetIsPerformerForMinter = useCallback(async () => {
+    const txHash = await isPerformerForMinter(
+      treatNftMinterContract,
+      modelAddress
+    );
+    console.log(txHash);
+    return txHash;
+  }, [account, treatNftMinterContract]);
+
+  return { onGetIsPerformerForMinter: handleGetIsPerformerForMinter };
+};
+
+export default useGetIsPerformer;
