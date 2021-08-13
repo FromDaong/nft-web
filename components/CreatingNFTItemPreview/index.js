4f242e59789ef09f8f4d5778f480e7b775a7591e
Luca <lucamarchal@gmail.com>
2021-08-13 17:10:25 +0100

    creator marketplace

---
+++ components/CreatingNFTItemPreview/index.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import Button from "react-bootstrap/Button";
+import Link from "next/link";
+
+let easing = [0.175, 0.85, 0.42, 0.96];
+
+const variants = {
+  initial: {
+    y: 150,
+    opacity: 0,
+  },
+  hidden: {
+    opacity: 0,
+    y: 150,
+    transition: {
+      duration: 0.1,
+      ease: easing,
+    },
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: easing,
+    },
+  },
+};
+
+const CreatingNFTItem = ({
+  data,
+  imageUrl,
+  buttonLabel,
+  buttonFunction,
+  isOwner,
+  price,
+  name,
+  owner,
+  quantity,
+  disableAnimations,
+}) => {
+  const [image, setBase64Image] = useState();
+  console.log({ data });
+
+  useEffect(() => {
+    (async () => {
+      // if (data.image) {
+      //   fetch(data.image)
+      //     .then((r) => r.text())
+      //     .then((blob) => {
+      //       setBase64Image(blob.replace(`"`, "").replace(/["']/g, ""));
+      //     });
+      // }
+    })();
+  }, [data]);
+
+  return (
+    <motion.div variants={disableAnimations ? {} : variants} layout>
+      <div className="nft-card m-0">
+        <div className="totw-tag-wrapper">
+          {isOwner ? (
+            <div className="totw-tag">MY NFT</div>
+          ) : (
+            data.totw && <div className="totw-tag">TOTW</div>
+          )}
+          {quantity > 1 && (
+            <div className="quantity-wrapper totw-tag">
+              Contains {quantity}x
+            </div>
+          )}
+        </div>
+        {/* <Link href={`/model/${data.name}`}>
+          <div className="profile-pic">
+            <img
+              src={
+                data.model_profile_pic ||
+                `data:image/svg+xml;utf8,${generateFromString(
+                  data.attributes[0].value
+                )}`
+              }
+            />
+          </div>
+        </Link> */}
+        <div className="img-container text-center text-lg-left d-flex justify-content-center align-items-center">
+          <div
+            style={{ background: `url(${imageUrl})`, backgroundColor: "#333" }}
+            className="dynamic-image"
+          />
+          {/* <div className="info-overlay">
+              <EyeSlash size={32} />
+              <div>Purchase to View</div>
+            </div>
+            <Blurhash
+              style={{
+                borderRadius: 8,
+                overflow: "hidden",
+                border: "3px solid #E795B6",
+              }}
+              hash={data.blurhash}
+              width={"100%"}
+              height={375}
+              resolutionX={32}
+              resolutionY={32}
+              punch={1}
+            /> */}
+        </div>
+        <div className="text-container container">
+          <div className="title-section">
+            <div className="title">{name}</div>
+            <div className="name">
+              {owner && <b>Creator: </b>}
+              {/* {data.attributes[0].value} */}
+            </div>
+            {owner && (
+              <div className="name">
+                <b>Owner: </b>
+                {owner.slice(0, 6) + "..." + owner.slice(-6)}
+              </div>
+            )}
+          </div>
+          <div className="stats">
+            <div className="stat">
+              <div className="number">{price}</div>
+              <div className="label">BNB</div>
+            </div>
+          </div>
+        </div>
+        {buttonLabel && buttonFunction && (
+          <div className="row">
+            <div className="col-lg-12 mt-3">
+              <span className="d-inline-block w-100">
+                <Button
+                  className="w-100"
+                  variant="secondary"
+                  onClick={buttonFunction}
+                >
+                  <b className="d-flex align-items-center justify-content-center">
+                    {buttonLabel}
+                  </b>
+                </Button>
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
+    </motion.div>
+  );
+};
+
+export default CreatingNFTItem;
---
+++ pages/creator-dashboard/create-nft.js
@@ -0,0 +1,189 @@
+import React, { useState } from "react";
+import toBuffer from "blob-to-buffer";
+import { Form, Button } from "react-bootstrap";
+import { useFormik, FieldArray, FormikProvider } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+import Hero from "../../components/Hero";
+import CreatingNFTItem from "../../components/CreatingNFTItem";
+import { useDropzone } from "react-dropzone";
+import { create } from "ipfs-http-client";
+import async from "async";
+
+const client = create("https://ipfs.infura.io:5001/api/v0");
+
+const CreateNFT = () => {
+  const [ipfsFiles, setIpfsFiles] = useState([]);
+  const router = useRouter();
+  const [success, setSuccess] = useState(false);
+
+  const onDrop = (files) => {
+    if (files && files.length > 0) {
+      async.map(
+        files,
+        (file, cb) => {
+          toBuffer(file, (err, buff) => {
+            console.log({ err, file });
+            if (err) return cb(err);
+            client.add(buff).then((results) => {
+              console.log("=> IPFS Dropzone added: ", results);
+              cb(null, `https://ipfs.infura.io/ipfs/${results.path}`);
+            });
+          });
+        },
+        (err, results) => {
+          if (err)
+            return console.error("=> IPFS Dropzone: IPFS Upload Error: ", err);
+          console.log({ results });
+          setIpfsFiles(results);
+        }
+      );
+    }
+  };
+
+  const {
+    acceptedFiles: files,
+    getRootProps,
+    getInputProps,
+  } = useDropzone({
+    onDrop,
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      nfts: ipfsFiles.map((file) => ({
+        name: "NFT name here",
+        list_price: 0,
+        description: "",
+        blurhash: false,
+        image: file,
+        max_supply: 1000000000,
+        external_url: "https://treatdao.com/",
+        model_handle: "@model",
+        model_profile_pic: "pic",
+        model_bnb_address: "0x123",
+      })),
+    },
+    enableReinitialize: true,
+    validateOnChange: false,
+    validateOnBlur: false,
+    validationSchema: Yup.object().shape({
+      id: Yup.number().required("Please add a nft id"),
+      name: Yup.string().required("Please add a name"),
+      list_price: Yup.string().required("Please add the NFT list price"),
+      description: Yup.string(),
+      external_url: Yup.string().required("Please add a external_url"),
+      blurhash: Yup.string().required("Please add a blurhash"),
+      image: Yup.string().required("Please add a image"),
+      max_supply: Yup.string().required("Please add a max supply"),
+      model_handle: Yup.string().required("Please add a model handle"),
+      model_profile_pic: Yup.string().required(
+        "Please add a model profile pic"
+      ),
+      model_bnb_address: Yup.string().required(
+        "Please add the model bnb address"
+      ),
+      master_password: Yup.string().required("Please add the master password"),
+    }),
+    handleChange: (c) => {
+      console.log({ c });
+    },
+    onSubmit: (values) => {
+      SubmitToServer();
+    },
+  });
+
+  const SubmitToServer = async () => {
+    try {
+      const res = await fetch(`/api/nft/create`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formik.values),
+      });
+      const resJSON = await res.json();
+
+      if (resJSON.error && resJSON.error.errors) {
+        console.log(resJSON.error);
+        const ogErrors = Object.assign({}, resJSON.error.errors);
+        Object.keys(ogErrors).map((e) => {
+          ogErrors[e] = resJSON.error.errors[e].message;
+        });
+        formik.setErrors(ogErrors);
+        formik.setSubmitting(false);
+      }
+
+      if (resJSON.success) {
+        setSuccess(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (success) return <div>Success.</div>;
+
+  return (
+    <FormikProvider value={formik}>
+      <div className="container">
+        <Hero
+          title="Create New NFTs"
+          subtitle="Complete this form carefully. Make sure you don't leave this page after submitting the creation transaction."
+        />
+
+        {(!formik.values.nfts || formik.values.nfts.length === 0) && (
+          <div className="white-tp-container p-4 row">
+            <div
+              className="dropzone justify-content-center flex"
+              {...getRootProps()}
+              style={{ minHeight: 200 }}
+            >
+              <input {...getInputProps()} />
+              <p className="mb-0 text-center" style={{ fontSize: "1.1em" }}>
+                Step 1: Drag 'n' drop your all your high resolution images here,{" "}
+                <br />
+                or click to here to select them
+              </p>
+            </div>
+          </div>
+        )}
+
+        <Form onSubmit={formik.handleSubmit}>
+          <FieldArray
+            name="nfts"
+            render={(arrayHelpers) =>
+              formik.values.nfts &&
+              formik.values.nfts.length > 0 &&
+              formik.values.nfts.map((nft, i) => (
+                <CreatingNFTItem
+                  formik={formik}
+                  index={i}
+                  key={nft.image}
+                  imageUrl={nft.image}
+                  handleChange={formik.handleChange}
+                  arrayHelpers={arrayHelpers}
+                />
+              ))
+            }
+          />
+          <div className="buttons row pt-4">
+            <div className="col-md-6 mt-2 text-center">
+              <Button variant="light w-100 py-2" onClick={() => router.back()}>
+                <b>BACK TO DASHBOARD</b>
+              </Button>
+            </div>
+            <div className="col-md-6  mt-2 text-center">
+              <Button variant="primary py-2 w-100">
+                <b>CREATE NFTs</b>
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </div>
+    </FormikProvider>
+  );
+};
+
+export default CreateNFT;
