8fbd46e6db450af87d9c1dc2f655b6698ea72e38
s-m-o-l <71986235+s-m-o-l@users.noreply.github.com>
2021-08-14 14:55:17 -0700

    Create AND Add in one tx Hooks
    they are back oh yeah bb

---
+++ hooks/useCreateAndAddNFTs.ts
@@ -0,0 +1,26 @@
+import { getCreatorMartContract, createAndAddNFTs } from "../treat/utils";
+import { useCallback } from "react";
+import useTreat from "./useTreat";
+import { useWallet } from "use-wallet";
+
+const useCreateAndAddNFTs = (maxSupplys: Array<number>, amounts: Array<number>, hexData: string) => {
+  const { account } = useWallet();
+  const treat = useTreat();
+  const creatorMartContract = getCreatorMartContract(treat);
+
+  const handleCreateAndAddNFTs = useCallback(async () => {
+    const txHash = await createAndAddNFTs(
+      creatorMartContract,
+      account,
+      maxSupplys,
+      amounts,
+      hexData
+    );
+    console.log(txHash);
+    return txHash;
+  }, [account, maxSupplys, amounts, hexData, creatorMartContract]);
+
+  return { onCreateAndAddNFTs: handleCreateAndAddNFTs };
+};
+
+export default useCreateAndAddNFTs;
