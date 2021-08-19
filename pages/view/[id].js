7902980e147cdd7833ac88f64d645f504f962a16
Luca <lucamarchal@gmail.com>
2021-08-19 14:16:52 +0100

    Free NFT

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -81,7 +81,7 @@ const RedeemButton = ({ onMintNft, remainingNfts, nftData, setShowModal }) => {
           </Spinner>
           <span>
             {confirmWallet
-              ? " Please confirm in your wallet"
+              ? " Please confirm in your wallet and wait"
               : "Please wait..."}
           </span>
         </div>
@@ -159,6 +159,12 @@ const ViewNFT = ({ nftData, image, account }) => {
   const remainingNfts = maxNftSupply.minus(mintedNfts);
   const { onMintNft: onMintTotwNft } = useMintNft(nftData.id, nftCost);
   const { onMintCreatorNft } = useMintCreatorNft(nftData.id, nftCost);
+
+  const [showModal, setShowModal] = useState(false);
+  const { onGetFreeTreat } = useGetFreeTreat(nftData.id, nftCost);
+  const { onGetFreeCreatorTreat } = useGetFreeCreatorTreat(nftData.id, nftCost);
+  const openOrders = useGetOpenOrdersForNft(nftData.id);
+
   const onMintNft = async () => {
     if (nftData.old_totw) {
       return await onMintTotwNft();
@@ -166,11 +172,14 @@ const ViewNFT = ({ nftData, image, account }) => {
       return await onMintCreatorNft();
     }
   };
-  const [showModal, setShowModal] = useState(false);
-  const { onGetFreeTreat } = useGetFreeTreat(nftData.id, nftCost);
-  const openOrders = useGetOpenOrdersForNft(nftData.id);
 
-  console.log({ openOrders });
+  const onMintFreeNft = async () => {
+    if (nftData.old_totw) {
+      return await onGetFreeTreat();
+    } else {
+      return await onGetFreeCreatorTreat();
+    }
+  };
 
   const historyEvents = nftData.mints.map((m) => {
     return {
@@ -245,9 +254,7 @@ const ViewNFT = ({ nftData, image, account }) => {
                 )}
               </div>
               <RedeemButton
-                onMintNft={
-                  nftData.list_price === 0 ? onGetFreeTreat : onMintNft
-                }
+                onMintNft={nftData.list_price === 0 ? onMintFreeNft : onMintNft}
                 remainingNfts={remainingNfts}
                 nftData={nftData}
                 setShowModal={setShowModal}
