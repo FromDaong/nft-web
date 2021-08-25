52cea871bbe9e85fbeac48cdb643d0feadbfab62
s-m-o-l <71986235+s-m-o-l@users.noreply.github.com>
2021-08-25 14:47:13 -0700

    Update utils.js
    fix getNftCreator to use proper getter "creators()"
    
    add getCreatorReferrer and addReferrerToMinter utils for hooks.

--- treat/utils.js
+++ treat/utils.js
@@ -358,10 +358,36 @@ export const getIsGiveAwayNft = async (creatorMart, nftId) => {
 
 export const getNftCreator = async (treatNFTMinter, nftId) => {
   try {
-    const modelAddress = await treatNFTMinter.methods.tokenModels(nftId).call();
+    const modelAddress = await treatNFTMinter.methods.creators(nftId).call();
     return new modelAddress();
   } catch {
     return new treatMart.address();
+  }
+};
+
+export const getCreatorReferrer = async (treatNFTMinter, creatorAddress) => {
+  try {
+    const refAddress = await treatNFTMinter.methods.referrers(creatorAddress).call();
+    return new refAddress();
+  } catch {
+    return new treatMart.address();
+  }
+};
+
+export const addReferrerToMinter = async (
+  treatNFTMinter,
+  account,
+  performerAddress,
+  referrerAddress
+) => {
+  try {
+    return await treatNFTMinter.methods
+      .addTreatReferrer(performerAddress, referrerAddress)
+      .send({ from: account, value: 0 });
+  } catch (e) {
+    console.log({ e });
+    console.error(e);
+    return undefined;
   }
 };
 
