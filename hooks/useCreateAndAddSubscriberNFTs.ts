e0bdffbdf0fa49fac222b9c69b9dad777dd2cbfa
s-m-o-l <71986235+s-m-o-l@users.noreply.github.com>
2021-09-03 03:45:59 -0700

    hooks for subs

---
+++ hooks/useCreateAndAddSubscriberNFTs.ts
@@ -0,0 +1,32 @@
+import { getSubscriberMartContract, createAndAddSubscriberNFTs } from "../treat/utils";
+import { useCallback } from "react";
+import useTreat from "./useTreat";
+import { useWallet } from "use-wallet";
+
+const useCreateAndAddSubscriberNFTs = (
+  maxSupplys: Array<number>,
+  amounts: Array<number>,
+  hexData: string
+) => {
+  const { account } = useWallet();
+  const treat = useTreat();
+  const subscriberMartContract = getSubscriberMartContract(treat);
+
+  const handleCreateAndAddSubscriberNFTs = useCallback(async () => {
+    console.log({ maxSupplys, amounts });
+    const res = await createAndAddSubscriberNFTs(
+      subscriberMartContract,
+      account,
+      maxSupplys,
+      amounts,
+      amounts.map(() => false),
+      hexData
+    );
+
+    return res;
+  }, [account, maxSupplys, amounts, hexData, subscriberMartContract]);
+
+  return { onCreateAndAddSubscriberNFTs: handleCreateAndAddSubscriberNFTs };
+};
+
+export default useCreateAndAddSubscriberNFTs;
