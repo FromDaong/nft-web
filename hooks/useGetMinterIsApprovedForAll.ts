baf0d864ca6fad804ff2f47e3c9fc69c7baa9757
Luca <lucamarchal@gmail.com>
2021-09-10 02:30:40 +0100

    fix

--- hooks/useGetMinterIsApprovedForAll.ts
+++ hooks/useGetMinterIsApprovedForAll.ts
@@ -1,41 +1,45 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from "react";
 
-import { useWallet } from 'use-wallet'
-import { provider } from 'web3-core'
+import { useWallet } from "use-wallet";
+import { provider } from "web3-core";
 
-import useBlock from './useBlock'
+import useBlock from "./useBlock";
 
-import { getIsApprovedForAll } from '../utils/erc1155'
-import useTreat from './useTreat'
-import { getTreatMarketplaceAddress, getTreatNFTMinterContract } from '../treat/utils'
+import { getIsApprovedForAll } from "../utils/erc1155";
+import useTreat from "./useTreat";
+import {
+  getTreatMarketplaceAddress,
+  getTreatNFTMinterContract,
+} from "../treat/utils";
 
 const useGetMinterIsApprovedForAll = (tokenAddress: string) => {
-  const [allowance, setAllowance] = useState(false)
-  const {
-    account,
-    ethereum,
-  }: { account: string; ethereum: provider } = useWallet()
-  const block = useBlock()
+  const [allowance, setAllowance] = useState(false);
+  const { account, ethereum } = useWallet();
+  const block = useBlock();
 
-  const treat = useTreat()
+  const treat = useTreat();
 
-  const treatNftMinterContract = getTreatNFTMinterContract(treat)
-  const treatMarketplaceAddress = getTreatMarketplaceAddress(treat)
+  const treatNftMinterContract = getTreatNFTMinterContract(treat);
+  const treatMarketplaceAddress = getTreatMarketplaceAddress(treat);
 
   const fetchAllowance = useCallback(async () => {
-    if(treat) {
-      const _allowance = await getIsApprovedForAll(treatNftMinterContract, treatMarketplaceAddress, account)
-      setAllowance(_allowance)
+    if (treat) {
+      const _allowance = await getIsApprovedForAll(
+        treatNftMinterContract,
+        treatMarketplaceAddress,
+        account
+      );
+      setAllowance(_allowance);
     }
-  }, [account, ethereum, tokenAddress])
+  }, [account, ethereum, tokenAddress]);
 
   useEffect(() => {
     if (account && ethereum) {
-      fetchAllowance()
+      fetchAllowance();
     }
-  }, [account, ethereum, setAllowance, block, tokenAddress])
+  }, [account, ethereum, setAllowance, block, tokenAddress]);
 
-  return allowance
-}
+  return allowance;
+};
 
-export default useGetMinterIsApprovedForAll
+export default useGetMinterIsApprovedForAll;
