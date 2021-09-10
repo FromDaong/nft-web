e858bcb75db9d2eb737aa28934dadc56b695c0ce
Luca <lucamarchal@gmail.com>
2021-09-10 02:30:40 +0100

    fix

--- hooks/useApprove.ts
+++ hooks/useApprove.ts
@@ -1,30 +1,27 @@
-
-import { useCallback } from 'react'
-import { useWallet } from 'use-wallet'
-import { provider } from 'web3-core'
-import { Contract } from 'web3-eth-contract'
-import { approve, getTreatMartContract } from '../treat/utils'
-import useTreat from './useTreat'
-
-
+import { useCallback } from "react";
+import { useWallet } from "use-wallet";
+import { provider } from "web3-core";
+import { Contract } from "web3-eth-contract";
+import { approve, getTreatMartContract } from "../treat/utils";
+import useTreat from "./useTreat";
 
 const useApprove = (treatMart: Contract) => {
-  const { account }: { account: string; ethereum: provider } = useWallet()
-  const treat = useTreat()
-  const treatMartContract = getTreatMartContract(treat)
+  const { account } = useWallet();
+  const treat = useTreat();
+  const treatMartContract = getTreatMartContract(treat);
 
   const handleApprove = useCallback(async () => {
     try {
-      const tx = await approve(treatMartContract, account)
-      console.log("tx ", tx)
-      return tx
+      const tx = await approve(treatMartContract, account);
+      console.log("tx ", tx);
+      return tx;
     } catch (e) {
-      console.log("errhandleApprove2 ", e)
-      return false
+      console.log("errhandleApprove2 ", e);
+      return false;
     }
-  }, [account, treatMartContract])
+  }, [account, treatMartContract]);
 
-  return { onApprove: handleApprove }
-}
+  return { onApprove: handleApprove };
+};
 
-export default useApprove
+export default useApprove;
--- hooks/useTokenBalance.ts
+++ hooks/useTokenBalance.ts
@@ -1,32 +1,29 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from "react";
 
-import BigNumber from 'bignumber.js'
-import { useWallet } from 'use-wallet'
-import { provider } from 'web3-core'
+import BigNumber from "bignumber.js";
+import { useWallet } from "use-wallet";
+import { provider } from "web3-core";
 
-import { getBalance } from '../utils/erc20'
-import useBlock from './useBlock'
+import { getBalance } from "../utils/erc20";
+import useBlock from "./useBlock";
 
 const useTokenBalance = (tokenAddress: string) => {
-  const [balance, setBalance] = useState(new BigNumber(0))
-  const {
-    account,
-    ethereum,
-  }: { account: string; ethereum: provider } = useWallet()
-  const block = useBlock()
+  const [balance, setBalance] = useState(new BigNumber(0));
+  const { account, ethereum } = useWallet();
+  const block = useBlock();
 
   const fetchBalance = useCallback(async () => {
-    const balance = await getBalance(ethereum, tokenAddress, account)
-    setBalance(new BigNumber(balance))
-  }, [account, ethereum, tokenAddress])
+    const balance = await getBalance(ethereum, tokenAddress, account);
+    setBalance(new BigNumber(balance));
+  }, [account, ethereum, tokenAddress]);
 
   useEffect(() => {
     if (account && ethereum) {
-      fetchBalance()
+      fetchBalance();
     }
-  }, [account, ethereum, setBalance, block, tokenAddress])
+  }, [account, ethereum, setBalance, block, tokenAddress]);
 
-  return balance
-}
+  return balance;
+};
 
-export default useTokenBalance
+export default useTokenBalance;
