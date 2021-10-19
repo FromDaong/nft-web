459817920ccf9d984b44d9f3f04a8f8b04209ca9
Luca <lucamarchal@gmail.com>
2021-10-19 20:31:00 +0100

    switch dbs

--- pages/index.js
+++ pages/index.js
@@ -162,7 +162,7 @@ const Home = () => {
                   </div>
                   <div className="col-md-6 mt-2">
                     <a
-                      href="https://pancakeswap.finance/info/token/0x01bd7acb6fF3B6Dd5aefA05CF085F2104f3fC53F"
+                      href="https://pancakeswap.finance/swap?inputCurrency=0x01bd7acb6ff3b6dd5aefa05cf085f2104f3fc53f"
                       target="_blank"
                     >
                       <Button variant="light w-100 py-2">
