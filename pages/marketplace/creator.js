39baade696d6448e77070d5b013086361f1fe57f
Luca <lucamarchal@gmail.com>
2021-10-20 01:19:09 +0100

    use extended search

--- pages/marketplace/creator.js
+++ pages/marketplace/creator.js
@@ -38,7 +38,7 @@ const Marketplace = ({ search }) => {
   const fuse = new Fuse(nftDataArray, {
     keys: ["name", "description", "model_handle", "tags"],
     shouldSort: false,
-    useExtendedMatch: true,
+    useExtendedSearch: true,
     includeScore: true,
   });
 
