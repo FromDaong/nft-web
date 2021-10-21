6c9d6887318dbc9c1051d5534af3097e304ef409
Luca <lucamarchal@gmail.com>
2021-10-21 01:37:05 +0100

    ready for prod

---
+++ pages/api/nft/pin-nfts.js
@@ -0,0 +1,47 @@
+import dbConnect from "../../../utils/dbConnect";
+import Model from "../../../models/Model";
+// import User from "../../../../models/User";
+import withSession from "../../../lib/session";
+
+dbConnect();
+
+export default async (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const Models = await Model.find();
+
+        const returnNFTs = await Promise.all(
+          Models.map(async (n) => {
+            // if (isNaN(n.id)) return;
+            const newnft = await Model.updateOne(
+              { _id: n._id },
+              {
+                profile_pic: n.profile_pic.replace(
+                  "https://ipfs.infura.io/ipfs/",
+                  "https://treatdao.mypinata.cloud/ipfs/"
+                ),
+                banner_pic: n.banner_pic.replace(
+                  "https://ipfs.infura.io/ipfs/",
+                  "https://treatdao.mypinata.cloud/ipfs/"
+                ),
+              }
+            );
+
+            return newnft;
+          })
+        );
+
+        res.status(200).json(returnNFTs);
+      } catch (error) {
+        console.log({ error });
+        res.status(400).json({ success: false, error: error });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+};
