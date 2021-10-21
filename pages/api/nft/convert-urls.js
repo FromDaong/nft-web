0bbdc9154fef5d2add626e7509905a5a62083666
Luca <lucamarchal@gmail.com>
2021-10-21 01:37:05 +0100

    ready for prod

--- pages/api/nft/convert-urls.js
+++ pages/api/nft/convert-urls.js
@@ -5,6 +5,10 @@ import withSession from "../../../lib/session";
 
 dbConnect();
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default async (req, res) => {
   const { method } = req;
 
@@ -14,18 +18,30 @@ export default async (req, res) => {
         const NFTs = await NFT.find();
 
         const returnNFTs = await Promise.all(
-          NFTs.map(async (n) => {
-            const newnft = await NFT.updateOne(
-              { id: n.id },
+          NFTs.map(async (n, i) => {
+            await sleep(i * 1000);
+            const result = await fetch(
+              `https://api.pinata.cloud/pinning/pinByHash`,
               {
-                image: n.image.replace(
-                  "https://ipfs.infura.io:5001/api/v0/cat/",
-                  "https://treatdao.mypinata.cloud/ipfs/"
-                ),
+                method: "POST",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                  pinata_api_key: "b949556813c4f284c550",
+                  pinata_secret_api_key:
+                    "7a7b755c9c067dedb142c2cb9e9c077aebf561b552c440bf67b87331bac32939",
+                },
+                body: JSON.stringify({
+                  hashToPin: n.image.replace(
+                    "https://treatdao.mypinata.cloud/ipfs/",
+                    ""
+                  ),
+                }),
               }
             );
-
-            return newnft;
+            console.log({ result });
+            const resJSON = await result.json();
+            return resJSON;
           })
         );
 
