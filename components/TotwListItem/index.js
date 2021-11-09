4e89c3db8ab131924d680891f518802331635570
Luca <lucamarchal@gmail.com>
2021-11-09 17:28:03 +0000

    test

--- components/TotwListItem/index.js
+++ components/TotwListItem/index.js
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, {
-  Pagination,
-  EffectCoverflow,
-  Navigation,
-  Autoplay,
-} from "swiper";
+import LazyLoad from "react-lazyload";
 import NFTListItem from "../NFTListItem";
 import useSWR from "swr";
-
-SwiperCore.use([Pagination, EffectCoverflow, Navigation, Autoplay]);
+import Link from "next/link";
 
 const TotwListItem = ({ modelData }) => {
   const { data: nftData } = useSWR(
@@ -20,56 +14,64 @@ const TotwListItem = ({ modelData }) => {
   console.log({ nftData });
 
   return (
-    <div className="totw-list-item-container">
-      <div className="top-bar">
-        <div className="totw-name">
-          <div
-            className="pic"
-            style={{ backgroundImage: `url(${modelData.profile_pic})` }}
-          ></div>
-          {modelData.username}
+    <LazyLoad>
+      <div className="totw-list-item-container mt-5">
+        <div className="top-bar">
+          <Link href={`/creator/${modelData.username}`}>
+            <div className="totw-name">
+              <div
+                className="pic"
+                style={{ backgroundImage: `url(${modelData.profile_pic})` }}
+              ></div>
+              {modelData.username}
+            </div>
+          </Link>
+          <div className="button">
+            <Button variant="primary py-2 px-4">
+              <b>Purchase Set</b>
+            </Button>
+          </div>
         </div>
-        <div className="button">
-          <Button variant="primary py-2 px-4">
-            <b>Purchase Set</b>
-          </Button>
+        <div className="nft-display-section">
+          <Swiper
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              1080: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1540: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1920: {
+                slidesPerView: 5,
+                spaceBetween: 40,
+              },
+            }}
+            // centeredSlides
+            style={{ width: "100%" }}
+            navigation
+            loop
+          >
+            {nftData &&
+              nftData.nfts &&
+              nftData.nfts.map((nft, i) => {
+                if (nft && nft.totw)
+                  return (
+                    <SwiperSlide className="slide" key={nft.id}>
+                      <NFTListItem data={nft} disableAnimations purple />
+                    </SwiperSlide>
+                  );
+                else return null;
+              })}
+          </Swiper>
         </div>
       </div>
-      <div className="nft-display-section">
-        <Swiper
-          modules={[Pagination, EffectCoverflow, Navigation, Autoplay]}
-          effect="coverflow"
-          spaceBetween={50}
-          slidesPerView={5}
-          style={{ width: "100%" }}
-          lazy={{
-            loadPrevNext: true,
-            loadOnTransitionStart: true,
-          }}
-          autoplay={{ disableOnInteraction: true, delay: 3000 }}
-          navigation
-          loop
-          coverflowEffect={{
-            rotate: 7, // Slide rotate in degrees
-            stretch: 50, // Stretch space between slides (in px)
-            depth: 100, // Depth offset in px (slides translate in Z axis)
-            slideShadows: false, // Enables slides shadows
-          }}
-        >
-          {nftData &&
-            nftData.nfts &&
-            nftData.nfts.map((nft, i) => {
-              if (nft && nft.totw)
-                return (
-                  <SwiperSlide className="slide" key={nft.id}>
-                    <NFTListItem data={nft} disableAnimations />
-                  </SwiperSlide>
-                );
-              else return null;
-            })}
-        </Swiper>
-      </div>
-    </div>
+    </LazyLoad>
   );
 };
 
