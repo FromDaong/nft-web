0730c2c33bb24c245e6b6501f48c9072fd2e2698
Luca <lucamarchal@gmail.com>
2021-11-17 21:21:59 +0000

    fix

--- hooks/useBuyMelonNft.ts
+++ hooks/useBuyMelonNft.ts
@@ -6,22 +6,18 @@ import useTreat from "./useTreat";
 import { useWallet } from "use-wallet";
 import bsc from "@binance-chain/bsc-use-wallet";
 
-const useBuyMelonNft = (
-  id: number,
-  treatCost: number,
-  useMelonMart: boolean = false
-) => {
+const useBuyMelonNft = (useMelonMart: boolean = false) => {
   const { account } = useWallet();
   const treat = useTreat();
   const melonMartContract = useMelonMart
     ? getMelonMartContract(treat)
     : getMelonMartContract(treat);
 
-  const handleMintNft = useCallback(async () => {
-    const txHash = await mintNft(melonMartContract, account, id, treatCost);
+  const handleBuyMelonNft = useCallback(async () => {
+    const txHash = await buyMelonNft(melonMartContract, account);
     console.log(txHash);
     return txHash;
-  }, [account, id, treatCost, melonMartContract]);
+  }, [account, melonMartContract]);
 
   return { onBuyMelonNft: handleBuyMelonNft };
 };
