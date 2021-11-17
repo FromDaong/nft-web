c741a57e0fe677dd7f1db0fbbca0f4797df1dc59
0x6e6f6c61 <92896870+0x6e6f6c61@users.noreply.github.com>
2021-11-17 13:40:20 -0800

    Create useGetPendingMelons.ts

---
+++ hooks/useGetPendingMelons.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from "react";
+import useTreat from "./useTreat";
+import { useWallet } from "use-wallet";
+import { getMasterMelonFarmerContract, getPendingMelons } from "../treat/utils";
+
+const useGetPendingMelons = (pid: number) => {
+  const { account } = useWallet();
+  const treat = useTreat();
+  const [pendingMelons, setPendingMelons] = useState(null);
+  const masterMelonFarmerContract = getMasterMelonFarmerContract(treat);
+
+  const fetchPendingMelons = useCallback(async () => {
+    const amount = await getPendingMelons(masterMelonFarmerContract, pid, account);
+
+    setPendingMelons(amount);
+  }, [account, masterMelonFarmerContract, pid]);
+
+  useEffect(() => {
+    if (treat) {
+      fetchPendingMelons();
+    }
+  }, [pid, treat]);
+
+  return pendingMelons;
+};
+
+export default useGetPendingMelons;
