7f8777910435aed5861b114f9cf8b595025d7fb4
Luca <lucamarchal@gmail.com>
2021-11-18 19:53:36 +0000

    fix

--- hooks/useHarvestFarm.ts
+++ hooks/useHarvestFarm.ts
@@ -1,18 +1,18 @@
-import { useCallback } from 'react'
+import { useCallback } from "react";
 import useTreat from "./useTreat";
 import { useWallet } from "use-wallet";
-import { getMasterMelonFarmerContract, harvestFarm } from '../treat/utils'
+import { getMasterMelonFarmerContract, harvestFarm } from "../treat/utils";
 
 const useHarvestFarm = (farmPid: number) => {
   const { account } = useWallet();
   const treat = useTreat();
-  const masterMelonFarmerContract = getMasterMelonFarmerContract(treat)
+  const masterMelonFarmerContract = getMasterMelonFarmerContract(treat);
 
   const handleHarvest = useCallback(async () => {
-    await harvestFarm(masterMelonFarmerContract, farmPid)
-  }, [account, farmPid, masterMelonFarmerContract])
+    await harvestFarm(masterMelonFarmerContract, farmPid, account);
+  }, [account, farmPid, masterMelonFarmerContract]);
 
-  return { onReward: handleHarvest }
-}
+  return { onReward: handleHarvest };
+};
 
-export default useHarvestFarm
+export default useHarvestFarm;
