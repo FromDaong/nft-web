d83f27cb9c86d0794d2e50239660b70028e1af72
Luca <lucamarchal@gmail.com>
2021-11-18 20:16:46 +0000

    add a gap

--- pages/farms/farmers-market.js
+++ pages/farms/farmers-market.js
@@ -116,7 +116,10 @@ const Marketplace = ({ search }) => {
               finalArray.length > 0 && (
                 <div className="d-flex justify-content-center flex-wrap w-100">
                   {finalArray.map((o, i) => (
-                    <div classname="p-1" style={{ width: 355 }}>
+                    <div
+                      classname="d-flex justify-content-center"
+                      style={{ width: 370 }}
+                    >
                       <NFTListItem data={o} disableAnimations />
                     </div>
                   ))}
