11f99589df2835fe8b69ebfa956941aeef176b96
Luca <lucamarchal@gmail.com>
2021-11-30 22:29:05 +0000

    quick node

--- pages/api/model/find-by-address/[address].js
+++ pages/api/model/find-by-address/[address].js
@@ -5,7 +5,9 @@ import TreatNFTMinterAbi from "../../../../treat/lib/abi/treatnftminter.json";
 
 dbConnect();
 
-const web3 = new Web3("https://bsc-dataseed2.defibit.io/");
+const web3 = new Web3(
+  "https://divine-restless-feather.bsc.quiknode.pro/f9ead03ddd05508e4fe1f6952eea26ac035c8408/"
+);
 
 const treatNFTMinter = new web3.eth.Contract(
   TreatNFTMinterAbi,
