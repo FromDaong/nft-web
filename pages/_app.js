1b8ef74d155814f13b07a19d5169f31fd8dc036c
Luca <lucamarchal@gmail.com>
2021-12-25 23:29:10 +0000

    christmas update

--- pages/_app.js
+++ pages/_app.js
@@ -32,6 +32,7 @@ import {
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
+import Snowfall from "react-snowfall";
 
 function MyApp({ Component, pageProps }) {
   const oldTokenBalance = useTokenBalance(
@@ -113,6 +114,10 @@ function MyApp({ Component, pageProps }) {
             content="Treat is an exclusive platform for creators to sell NFTs. Hold $TREAT to have a say on which creators are chosen & new platform features."
           />
         </Head>
+        <Snowfall
+          snowflakeCount={250}
+          color={"#FE93B9"}
+        />
         <SWRConfig
           value={{
             fetcher: fetch,
