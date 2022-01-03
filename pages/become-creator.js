a4c6e5a2b729a8ad7468253631f8677296a9f65b
tomide <ayotomiiide@gmail.com>
2022-01-03 12:25:34 +0000

    ft: move verification to another step

--- pages/become-creator.js
+++ pages/become-creator.js
@@ -17,6 +17,7 @@ import { create } from "ipfs-http-client";
 import { useWallet } from "use-wallet";
 
 import dynamic from "next/dynamic";
+import { motion } from "framer-motion";
 
 const VerifyButton = dynamic(() => import("@passbase/button/react"), {
   ssr: false,
@@ -24,10 +25,10 @@ const VerifyButton = dynamic(() => import("@passbase/button/react"), {
 
 const client = create("https://ipfs.infura.io:5001/api/v0");
 
-
 const CreateModel = () => {
   const router = useRouter();
   const [success, setSuccess] = useState(false);
+  const [step, setStep] = useState("signup");
   const { account } = useWallet();
   const { data: res } = useSWR(`/api/model/find-by-address/${account}`);
 
@@ -79,7 +80,7 @@ const CreateModel = () => {
       }
 
       if (resJSON.success) {
-        setSuccess(true);
+        setStep("verify");
       }
     } catch (error) {
       console.log(error);
@@ -100,9 +101,13 @@ const CreateModel = () => {
     });
   };
 
+  const handleVerificationFinish = (identityAccessKey) => {
+    console.log("identityAccessKey", identityAccessKey);
+  };
+
   console.log({ res });
 
-  if (success || (res && res.pending))
+  if (success || (res && res.pending && res.identity_access_key.length > 0))
     return (
       <Hero
         title="Your application has been submitted!"
@@ -113,195 +118,199 @@ const CreateModel = () => {
   if (res && res.rejected)
     return <Hero title="Your application has been rejected" />;
 
+  useEffect(() => {
+    if (res && res.identity_access_key.length === 0) {
+      setStep("verify");
+    }
+  }, [res]);
+
   return (
     <div className="no-position" style={{ maxWidth: 800, margin: "auto" }}>
-      <Hero
-        title="Become a Creator"
-        subtitle="Complete the form below to apply to become a creator. Creators are able to mint NFTs on TreatDAO!"
-      />
-      <div
-        className="container p-4 pb-0 white-tp-container-no-filter"
-        style={{ borderRadius: 10 }}
-      >
-        <Form onSubmit={formik.handleSubmit}>
-          <div className="pb-4">
-            <div className="pb-4">
-              <label>Username</label>
-              <FormControl
-                placeholder="E.g. alexanbt"
-                name="username"
-                value={formik.values.username}
-                onChange={(e) =>
-                  formik.setFieldValue(
-                    "username",
-                    e.target.value.replace(" ", "")
-                  )
-                }
-              />
-              <small>treatdao.com/creator/{formik.values.username}</small>
-            </div>
-
-            <div className="pb-3">
-              <label>Profile bio / about you</label>
-              <FormControl
-                as="textarea"
-                name="bio"
-                value={formik.values.bio}
-                onChange={formik.handleChange}
-              />
-              <small>
-                {formik.values.bio ? formik.values.bio.length : 0} / 1000
-              </small>
-            </div>
-            <div className="pb-4 pt-1">
-              <label>Email:</label>
-
-              <FormControl
-                name="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-              />
-            </div>
-            <div className="pb-4 pt-1">
-              <label>Referrer address (optional):</label>
-              <FormControl
-                name="referrer_address"
-                value={formik.values.referrer_address}
-                onChange={formik.handleChange}
-              />
-              <small>
-                If someone referred you to Treat, put their wallet address here
-                so they are rewarded.
-              </small>
-              {/* <FormControl
-                type="file"
-                size="lg"
-                placeholder="E.g. https://img.ur/123"
-                name="verification_photo"
-                className="bg-white p-3 rounded"
-                // value={formik.values.verification_photo}
-                onChange={(file) =>
-                  ipfsUpload(file.target.files[0], "verification_photo")
-                }
-              /> */}
-            </div>
-            <div className="pb-4">
-              <label>URL / Link to an existing social account</label>
-              <FormControl
-                placeholder="https://twitter.com/alexanbt"
-                name="social_account"
-                value={formik.values.social_account}
-                onChange={formik.handleChange}
-              />
-              <small className="text-danger">
-                {formik.errors["social_account"]}
-              </small>
-            </div>
-            <div className="pb-4">
-              <label className="m-0">Profile Picture</label>
-              <br />
-              <small>
-                Please ensure photo is square, 1000 x 1000 recommended.
-              </small>
-              <FormControl
-                type="file"
-                size="lg"
-                placeholder="E.g. https://img.ur/123"
-                name="profile_pic"
-                className="bg-white p-3 rounded"
-                // value={formik.values.profile_pic}
-                onChange={(file) =>
-                  ipfsUpload(file.target.files[0], "profile_pic")
-                }
-              />
-            </div>
-            <div className="pb-4 pt-2 verify-container">
-              <label>Verify your identity</label>
-
-              <VerifyButton
-                id="asd"
-                // hidestream
-                apiKey="zYe9VHCjf5z2MoiuQGlvRK3KRPmQ0B7Kaghp6qyFZ8PfTnQa0zFRuZZWgoVGeAVX"
-                onStart={() => {}}
-                onError={(errorCode) => {}}
-                onFinish={(identityAccessKey) => {
-                  formik.setFieldValue(
-                    "identity_access_key",
-                    identityAccessKey
-                  );
-                }}
-              />
-
-              {/* <small>
-                <b className="text-danger">
-                  Please ensure you return to submit this form after you have
-                  completed ID verification with passbase.
-                </b>
-              </small> */}
-              {/* <FormControl
-                type="file"
-                size="lg"
-                placeholder="E.g. https://img.ur/123"
-                name="verification_photo"
-                className="bg-white p-3 rounded"
-                // value={formik.values.verification_photo}
-                onChange={(file) =>
-                  ipfsUpload(file.target.files[0], "verification_photo")
-                }
-              /> */}
-            </div>
-
-            <div className="pb-4">
-              <br />
-              <FormGroup>
-                <FormCheck>
-                  <Form.Check.Input
-                    type="checkbox"
-                    isValid={formik.values.terms_accepted}
+      {step === "signup" && (
+        <motion.div
+          animate={{ y: 0, opacity: 1 }}
+          style={{ y: -100, opacity: 0 }}
+          transition={{ delay: 0.25 }}
+          className="pink-bg mb-5"
+        >
+          <Hero
+            title="Become a Creator"
+            subtitle="Complete the form below to apply to become a creator. Creators are able to mint NFTs on TreatDAO!"
+          />
+          <div
+            className="container p-4 pb-0 white-tp-container-no-filter"
+            style={{ borderRadius: 10 }}
+          >
+            <Form onSubmit={formik.handleSubmit}>
+              <div className="pb-4">
+                <div className="pb-4">
+                  <label>Username</label>
+                  <FormControl
+                    placeholder="E.g. alexanbt"
+                    name="username"
+                    value={formik.values.username}
                     onChange={(e) =>
-                      formik.setFieldValue("terms_accepted", e.target.checked)
+                      formik.setFieldValue(
+                        "username",
+                        e.target.value.replace(" ", "")
+                      )
                     }
                   />
-                  <FormCheck.Label>
-                    <label className="black">
-                      I agree to the{" "}
-                      <a
-                        href="https://drive.google.com/file/d/1Li5EAK8sP71rY1wT9J87mUCjLWmjcO2a/view?usp=sharing"
-                        style={{
-                          color: "inherit",
-                          textDecoration: "underline",
-                        }}
-                        target="_blank"
-                      >
-                        terms of service.
-                      </a>
-                    </label>
-                  </FormCheck.Label>
-                </FormCheck>
-                <small className="text-danger">
-                  {formik.errors["terms_accepted"]}
-                </small>
-              </FormGroup>
-            </div>
+                  <small>treatdao.com/creator/{formik.values.username}</small>
+                </div>
 
-            <Button
-              variant="primary w-100"
-              onClick={formik.handleSubmit}
-              type="submit"
-            >
-              Submit Application
-            </Button>
-            {Object.keys(formik.errors).length > 0 && (
-              <Form.Control.Feedback type="invalid" className="d-block">
-                {Object.keys(formik.errors).map((e) => (
-                  <div>{formik.errors[e]}</div>
-                ))}
-                {formik.errors.code}
-              </Form.Control.Feedback>
-            )}
+                <div className="pb-3">
+                  <label>Profile bio / about you</label>
+                  <FormControl
+                    as="textarea"
+                    name="bio"
+                    value={formik.values.bio}
+                    onChange={formik.handleChange}
+                  />
+                  <small>
+                    {formik.values.bio ? formik.values.bio.length : 0} / 1000
+                  </small>
+                </div>
+                <div className="pb-4 pt-1">
+                  <label>Email:</label>
+
+                  <FormControl
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+                <div className="pb-4 pt-1">
+                  <label>Referrer address (optional):</label>
+                  <FormControl
+                    name="referrer_address"
+                    value={formik.values.referrer_address}
+                    onChange={formik.handleChange}
+                  />
+                  <small>
+                    If someone referred you to Treat, put their wallet address
+                    here so they are rewarded.
+                  </small>
+                </div>
+                <div className="pb-4">
+                  <label>URL / Link to an existing social account</label>
+                  <FormControl
+                    placeholder="https://twitter.com/alexanbt"
+                    name="social_account"
+                    value={formik.values.social_account}
+                    onChange={formik.handleChange}
+                  />
+                  <small className="text-danger">
+                    {formik.errors["social_account"]}
+                  </small>
+                </div>
+                <div className="pb-4">
+                  <label className="m-0">Profile Picture</label>
+                  <br />
+                  <small>
+                    Please ensure photo is square, 1000 x 1000 recommended.
+                  </small>
+                  <FormControl
+                    type="file"
+                    size="lg"
+                    placeholder="E.g. https://img.ur/123"
+                    name="profile_pic"
+                    className="bg-white p-3 rounded"
+                    // value={formik.values.profile_pic}
+                    onChange={(file) =>
+                      ipfsUpload(file.target.files[0], "profile_pic")
+                    }
+                  />
+                </div>
+
+                <div className="pb-4">
+                  <FormGroup>
+                    <FormCheck>
+                      <Form.Check.Input
+                        type="checkbox"
+                        isValid={formik.values.terms_accepted}
+                        onChange={(e) =>
+                          formik.setFieldValue(
+                            "terms_accepted",
+                            e.target.checked
+                          )
+                        }
+                      />
+                      <FormCheck.Label>
+                        <label className="black">
+                          I agree to the{" "}
+                          <a
+                            href="https://drive.google.com/file/d/1Li5EAK8sP71rY1wT9J87mUCjLWmjcO2a/view?usp=sharing"
+                            style={{
+                              color: "inherit",
+                              textDecoration: "underline",
+                            }}
+                            target="_blank"
+                          >
+                            terms of service.
+                          </a>
+                        </label>
+                      </FormCheck.Label>
+                    </FormCheck>
+                    <small className="text-danger">
+                      {formik.errors["terms_accepted"]}
+                    </small>
+                  </FormGroup>
+                </div>
+
+                <Button
+                  variant="primary w-100"
+                  onClick={formik.handleSubmit}
+                  type="submit"
+                >
+                  Submit Application
+                </Button>
+                {Object.keys(formik.errors).length > 0 && (
+                  <Form.Control.Feedback type="invalid" className="d-block">
+                    {Object.keys(formik.errors).map((e) => (
+                      <div>{formik.errors[e]}</div>
+                    ))}
+                    {formik.errors.code}
+                  </Form.Control.Feedback>
+                )}
+              </div>
+            </Form>
           </div>
-        </Form>
-      </div>
+        </motion.div>
+      )}
+
+      {step === "verify" && (
+        <motion.div
+          animate={{ y: 0, opacity: 1 }}
+          style={{ y: -100, opacity: 0 }}
+          transition={{ delay: 0.25 }}
+          className="pink-bg mb-5"
+        >
+          <Hero
+            title="Become a Creator"
+            subtitle="Verfiy your identity to apply to become a creator. Creators are able to mint NFTs on TreatDAO!"
+          />
+          <div
+            className="container p-4 pb-0 white-tp-container-no-filter"
+            style={{ borderRadius: 10 }}
+          >
+            <div className="pb-4">
+              <div className="pb-4 pt-2 verify-container">
+                <label>Click the button below to verify your identity</label>
+
+                <VerifyButton
+                  id="asd"
+                  // hidestream
+                  apiKey="zYe9VHCjf5z2MoiuQGlvRK3KRPmQ0B7Kaghp6qyFZ8PfTnQa0zFRuZZWgoVGeAVX"
+                  onStart={() => {}}
+                  onError={(errorCode) => {}}
+                  onFinish={handleVerificationFinish}
+                />
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      )}
     </div>
   );
 };
