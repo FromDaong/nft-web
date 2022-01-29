7a56b8d9a9a7425ce85f6b0a1d43c84cd42079e7
Kamfeskaya <chris@devtachom.com>
2022-01-29 08:43:27 +0200

    feat: add dropdown to [id]

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -31,6 +31,7 @@ import {
 } from "react-bootstrap-icons";
 import BigNumber from "bignumber.js";
 import Link from "next/link";
+import Dropdown from "react-bootstrap/Dropdown";
 import useGetIsSubscribed from "../../hooks/useGetIsSubscribed";
 import { gql, useQuery } from "@apollo/client";
 
@@ -182,6 +183,7 @@ const ViewNFT = ({ nftData, image, account }) => {
   const totwNftCost = useGetTreatNFTCost(nftData.id);
   const creatorNftCost = getCreatorNftCost(nftData.id);
   const subscriberNftCost = getSubscriberNftCost(nftData.id);
+  const [sortBy, setSortBy] = useState("Recent");
 
   let nftCost = nftData.old_totw ? totwNftCost : creatorNftCost;
   nftCost = nftData.subscription_nft ? subscriberNftCost : nftCost;
@@ -202,6 +204,7 @@ const ViewNFT = ({ nftData, image, account }) => {
   );
 
   const openOrders = useGetOpenOrdersForNft(nftData.id) ?? [];
+  const [finalArray, setFinalArray] = useState([]);
   // Get lowest price value in open orders
   const lowestOpenOrder = new BigNumber(openOrders.reduce(
     (lowest, order, index) =>
@@ -287,7 +290,7 @@ const ViewNFT = ({ nftData, image, account }) => {
     return aDate > bDate ? -1 : 1;
   });
   const loadingHistory = loadingMintHistory && loadingResaleHistory
-  console.log({allData, resaleHistoryData, mintHistoryData, openOrders})
+  console.log({allData, finalArray, openOrders})
 
   const onMintNft = async () => {
     if (nftData.subscription_nft) return onMintSubscriberNft();
@@ -314,7 +317,26 @@ const ViewNFT = ({ nftData, image, account }) => {
     };
   });
 
-  console.log({allData})
+  const setSort = (sortBy) => {
+    setSortBy(sortBy);
+  };
+
+  useEffect(() => {
+    const newArray = openOrders;
+    newArray.sort((a, b) => {
+        switch (sortBy) {
+          case "Recent":
+            return Number(a.listDate) - Number(b.listDate);
+          case "Price":
+            return Number(a.price) - Number(b.price);
+          default:
+            return Number(a.listDate) - Number(b.listDate);
+        }
+    })
+    setFinalArray(newArray);
+  }, [openOrders, sortBy]);
+
+
   const purchaseHistoryRender = 
     allData.map((e) => (
       <div className="history-event d-flex justify-content-between">
@@ -345,7 +367,7 @@ const ViewNFT = ({ nftData, image, account }) => {
     ));
 
   // Sort with lowest first
-  const openOrdersRender = openOrders.map((e) => (
+  const openOrdersRender = finalArray.map((e) => (
     <Link href={`/marketplace/resale?search=${nftData.name}`} passHref={true}>
       <a>
         <div className="history-event">
@@ -493,9 +515,30 @@ const ViewNFT = ({ nftData, image, account }) => {
                     <div className="history-title text-center mt-2">
                       Resale Marketplace Listings
                     </div>
-                    <div className="bio text-center">
-                      Total currently listed:{" "}
+                    <div className="bio" style={{display: "flex", justifyContent: "space-between"}}>
+                      <p>
+                        Total currently listed:{" "}
                       {openOrdersRender && openOrdersRender.length}
+                      </p>
+                      <div>
+                        <Dropdown>
+                          <Dropdown.Toggle
+                            variant="transparent"
+                            id="dropdown-basic"
+                            size="lg"
+                          >
+                            {sortBy}
+                          </Dropdown.Toggle>
+                          <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => setSort("Recent")}>
+                              Most Recent
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={() => setSort("Price")}>
+                              Price Low to High
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </div>
                     </div>
                     {/* <div className="bio">Coming soon...</div> */}
                     <div className="history-events">{openOrdersRender}</div>
