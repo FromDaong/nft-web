c483208ae1b6464a3cdc81717c93a9828678ea01
Kamfeskaya <chris@devtachom.com>
2022-02-01 18:41:21 +0200

    feat: add grid list with search

--- components/Lists/GridListWithSearch.tsx
+++ components/Lists/GridListWithSearch.tsx
@@ -1,11 +1,19 @@
 import { FC, ReactNode } from "react";
 
 export default function GridListWithSearch(props: GridListProps) {
-  return <div></div>;
+  return (
+    <div className="p-2">
+      <div className="pb-4"></div>
+      <div className={`grid grid-cols-${props.cols ? props.cols : 4}`}>
+        {props.data.map((item) => props.render(item))}
+      </div>
+    </div>
+  );
 }
 
 interface GridListProps {
   searchPlaceholder: string;
   render: (props) => ReactNode;
   data: Array<any>;
+  cols?: Number;
 }
