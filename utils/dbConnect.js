1f1a73ab7da951b95b62911a87019558add52615
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-09 15:20:45 +0200

    feat: use middleware

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -17,4 +17,21 @@ async function dbConnect() {
   connection.isConnected = db.connections[0].readyState;
 }
 
+const connectDB = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
+  // Use new db connection
+  await mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  });
+  return handler(req, res);
+};
+
+export { connectDB };
+
 export default dbConnect;
