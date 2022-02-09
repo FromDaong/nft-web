68d94caec0f2605b5b124ee0756b26c7f21c0fee
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-09 15:07:55 +0200

    feat: use prod force

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -6,10 +6,14 @@ async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const db = await mongoose.connect(
+    "mongodb+srv://vercel:JQc7KIP793qFXFC5@treatcluster.9uuso.mongodb.net/treat?retryWrites=true&w=majority",
+    // process.env.MONGO_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
   connection.isConnected = db.connections[0].readyState;
 }
 
