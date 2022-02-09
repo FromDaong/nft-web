f3bc60ca0d8a2625f6f4173dbd4998793529a459
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-09 09:28:00 +0200

    fix: add dbConnect to autocomplete

--- pages/api/v2/search/autocomplete.js
+++ pages/api/v2/search/autocomplete.js
@@ -1,56 +1,64 @@
 import Model from "../../../../models/Model";
 import NFT from "../../../../models/NFT";
+import dbConnect from "../../../../utils/dbConnect";
+
+dbConnect();
 
 export default async function autocomplete(req, res) {
   const { s } = req.query;
   const { group } = req.query;
 
-  if (!s) {
-    return res.json([]);
-  }
-
-  const nfts = await NFT.aggregate([
-    {
-      $search: {
-        autocomplete: {
-          query: s,
-          path: ["name", "description", "model_handle"],
-          fuzzy: {
-            maxEdits: 2,
-            prefixLength: 3,
-          },
-        },
-      },
-    },
-  ]);
-  const models = await Model.aggregate([
-    {
-      $search: {
-        autocomplete: {
-          query: s,
-          path: ["username", "bio", "display_name"],
-          fuzzy: {
-            maxEdits: 2,
-            prefixLength: 3,
-          },
-        },
-      },
-    },
-  ]);
-
-  if (group) {
-    switch (group) {
-      case "nfts":
-        return res.json(nfts);
-      case "models":
-        return res.json(models);
-      default:
-        return res.json([]);
+  try {
+    if (!s) {
+      return res.json([]);
     }
-  }
 
-  return res.json([
-    ...nfts.map((nft) => ({ ...nft, group: "nft" })),
-    ...models.map((model) => ({ ...model, group: "model" })),
-  ]);
+    const nfts = await NFT.aggregate([
+      {
+        $search: {
+          autocomplete: {
+            query: s,
+            path: ["name", "description", "model_handle"],
+            fuzzy: {
+              maxEdits: 2,
+              prefixLength: 3,
+            },
+          },
+        },
+      },
+    ]);
+    const models = await Model.aggregate([
+      {
+        $search: {
+          autocomplete: {
+            query: s,
+            path: ["username", "bio", "display_name"],
+            fuzzy: {
+              maxEdits: 2,
+              prefixLength: 3,
+            },
+          },
+        },
+      },
+    ]);
+
+    if (group) {
+      switch (group) {
+        case "nfts":
+          return res.json(nfts);
+        case "models":
+          return res.json(models);
+        default:
+          return res.json([]);
+      }
+    }
+
+    return res.json([
+      ...nfts.map((nft) => ({ ...nft, group: "nft" })),
+      ...models.map((model) => ({ ...model, group: "model" })),
+    ]);
+  } catch (error) {
+    console.error({ error });
+    res.status(400).json({ success: false, error: error });
+  }
 }
