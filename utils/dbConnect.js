e7d4c3b5033341183ea12de9ed8a45c96056d6f3
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-09 15:38:35 +0200

    fix: reset connection string

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -6,32 +6,11 @@ async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(
-    "mongodb+srv://vercel:JQc7KIP793qFXFC5@treatcluster.9uuso.mongodb.net/treat?retryWrites=true&w=majority",
-    // process.env.MONGO_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  const db = await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   connection.isConnected = db.connections[0].readyState;
 }
-
-const connectDB = (handler) => async (req, res) => {
-  if (mongoose.connections[0].readyState) {
-    // Use current db connection
-    return handler(req, res);
-  }
-  // Use new db connection
-  await mongoose.connect(process.env.MONGO_URL, {
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-  });
-  return handler(req, res);
-};
-
-export { connectDB };
 
 export default dbConnect;
