d30f78a7160a3fed818e3693163788986222c292
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-17 08:03:11 +0200

    feat: add daoCdnUrl

--- pages/marketplace/creator.js
+++ pages/marketplace/creator.js
@@ -135,12 +135,7 @@ const Marketplace = ({ search }) => {
 
   return (
     <AnimateSharedLayout>
-      <motion.main
-        variants={{
-          hidden: { opacity: 0, x: -200, y: 0 },
-          enter: { opacity: 1, x: 0, y: 0 },
-          exit: { opacity: 0, x: 0, y: -100 },
-        }}
+      <div
         initial="hidden" // Set the initial state to variants.hidden
         animate="enter" // Animated state to variants.enter
         exit="exit" // Exit state (used later) to variants.exit
@@ -298,7 +293,7 @@ const Marketplace = ({ search }) => {
             />
           </div>
         </div>
-      </motion.main>
+      </div>
     </AnimateSharedLayout>
   );
 };
