e3df9e4d97540d3a266a03655716617b6610322a
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-17 08:03:11 +0200

    feat: add daoCdnUrl

--- pages/marketplace/resale.js
+++ pages/marketplace/resale.js
@@ -188,18 +188,7 @@ const Marketplace = ({ search }) => {
         account={account}
       />
 
-      <motion.main
-        variants={{
-          hidden: { opacity: 0, x: -200, y: 0 },
-          enter: { opacity: 1, x: 0, y: 0 },
-          exit: { opacity: 0, x: 0, y: -100 },
-        }}
-        initial="hidden" // Set the initial state to variants.hidden
-        animate="enter" // Animated state to variants.enter
-        exit="exit" // Exit state (used later) to variants.exit
-        transition={{ type: "linear" }} // Set the transition to linear
-        className=""
-      >
+      <div className="">
         <Hero
           title={"Resale Marketplace"}
           subtitle={`The brand new official Treat resale marketplaces!`}
@@ -337,7 +326,7 @@ const Marketplace = ({ search }) => {
             />
           </div>
         </div>
-      </motion.main>
+      </div>
     </AnimateSharedLayout>
   );
 };
