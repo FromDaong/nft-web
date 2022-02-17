53391aed31ebef327a2ac6ed717f10dd4b400115
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-17 08:36:16 +0200

    fix: remove 0 opacity from hero

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 const Hero = ({ title, subtitle, additionalContent, titleClass }) => {
   return (
-    <div
-      animate={{ y: 0, opacity: 1 }}
-      style={{ y: -100, opacity: 0 }}
-      transition={{ delay: 0.25 }}
-      className="pink-bg mb-5"
-    >
+    <div className="pink-bg mb-5">
       <div
         className={`heading-text p-0 mt-5 ${titleClass}`}
         style={{ fontSize: "3.5em", lineHeight: 1.2 }}
