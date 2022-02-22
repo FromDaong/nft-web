2db48c49424f365c51e91ca23986fa3a37e1af4f
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2022-02-22 18:10:10 +0200

    fix: fix to load all

--- pages/api/v2/utils/images/fetchWithFallback.ts
+++ pages/api/v2/utils/images/fetchWithFallback.ts
@@ -3,13 +3,27 @@ import axios from "axios";
 // Checks if image does not return error from CDN
 // If it does it then downloads and sends the base64 encoded image from pinata
 export default async function fetchWithFallback(req, res) {
+  if (!req.query.cdn) {
+    try {
+      if (req.query.default.includes("mypinata")) {
+        return res.send(req.query.default);
+      } else {
+        const cdnurl = `${req.query.default}-/quality/lighter/-/format/webp/`;
+        await axios.get(cdnurl);
+        return res.send(cdnurl);
+      }
+    } catch (err) {
+      return res.send(req.query.default);
+    }
+  }
   try {
-    await axios.get(req.query.cdn);
-    res.send(`${req.query.cdn}-/quality/lighter/-/format/webp/`);
+    const cdnurl = `${req.query.cdn}-/quality/lighter/-/format/webp/`;
+    await axios.get(cdnurl);
+
+    return res.send(cdnurl);
   } catch (err) {
     // const resp = await axios.get(req.query.default);
     // const blob = data.replace(`"`, "").replace(/["']/g, "");
-
     return res.send(req.query.default);
   }
 }
