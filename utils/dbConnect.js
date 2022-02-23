5b73481930112fa655549af38ed525a029e31d28
tate2301 <chris@devtachom.com>
2022-02-23 15:57:24 +0000

    fix: fix to use env

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI =
-  "mongodb+srv://vercel:JQc7KIP793qFXFC5@treatcluster.9uuso.mongodb.net/treat?retryWrites=true&w=majority"; // process.env.MONGO_URL;
+const MONGODB_URI = process.env.MONGO_URL;
 
 if (!MONGODB_URI) {
   throw new Error(
