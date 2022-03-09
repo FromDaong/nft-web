ba023fb7064edae499042a5aa429f1a9d4f81ab5
Kamfeskaya <chris@devtachom.com>
2022-03-09 19:19:29 +0200

    fix: fix base64 conversion

--- pages/api/v2/utils/images/fetchWithFallback.ts
+++ pages/api/v2/utils/images/fetchWithFallback.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
+const atob = require("atob");
 const sharp = require("sharp");
+
+function dataURLtoFile(dataurl) {
+  const arr = dataurl.split(",");
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return Buffer.from(u8arr);
+}
 
 // Checks if image does not return error from CDN
 // If it does it then downloads and sends the base64 encoded image from pinata
 export default async function fetchWithFallback(req, res) {
-  try {
-    if (req.query.default.includes("mypinata")) {
+  if (req.query.default.includes("mypinata")) {
+    try {
       const response = await axios.get(req.query.default, {
         responseType: "arraybuffer",
       });
@@ -15,22 +30,37 @@ export default async function fetchWithFallback(req, res) {
         .toBuffer();
       res.setHeader("Content-Type", "image/webp");
       return res.send(image);
-    } else {
+    } catch (err) {
+      console.log({ err });
+      const response = await axios.get(req.query.default);
+
+      const blob = dataURLtoFile(response.data); //response.data.replace(`"`, "").replace(/["']/g, "");
+      const image = await sharp(blob)
+        .resize(500)
+        .toFormat("webp", { nearLossless: true, quality: 50 })
+        .toBuffer();
+      res.setHeader("Content-Type", "image/webp");
+      return res.send(image);
+    }
+  } else {
+    try {
       const cdnurl = `${req.query.default}-/quality/lighter/-/format/webp/`;
-      console.log(cdnurl);
       const image_data = await axios.get(cdnurl);
       res.setHeader("Content-Type", "image/webp");
       return res.send(image_data);
+    } catch (err) {
+      console.log({ err });
+      const response = await axios.get(req.query.default, {
+        responseType: "arraybuffer",
+      });
+
+      const blob = response.data.replace(`"`, "").replace(/["']/g, "");
+      const image = await sharp(Buffer.from(blob))
+        .resize(500)
+        .toFormat("webp", { nearLossless: true, quality: 50 })
+        .toBuffer();
+      res.setHeader("Content-Type", "image/webp");
+      return res.send(image);
     }
-  } catch (err) {
-    const response = await axios.get(req.query.default, {
-      responseType: "arraybuffer",
-    });
-    const image = await sharp(Buffer.from(response.data))
-      .resize(500)
-      .toFormat("webp", { nearLossless: true, quality: 50 })
-      .toBuffer();
-    res.setHeader("Content-Type", "image/webp");
-    return res.send(image);
   }
 }
