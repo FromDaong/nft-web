92e052e59c95157298e799a8cd92d9a61e39de07
Kamfeskaya <chris@devtachom.com>
2022-03-21 18:21:47 +0200

    fix: render icons as svg img

--- components/icons/Photograph.jsx
+++ components/icons/Photograph.jsx
@@ -1,5 +1,3 @@
-import NFT from "./NFTS.svg";
-
 export default function Photograph(props) {
-  return <NFT {...props} />;
+  return <img src="/assets/nfts.svg" className="h-4 w-4" {...props} />;
 }
