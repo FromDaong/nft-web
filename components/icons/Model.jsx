698368869c63958b240008c7d41275003b9a7448
Kamfeskaya <chris@devtachom.com>
2022-03-27 09:57:44 +0200

    fix: style search bar

--- components/icons/Model.jsx
+++ components/icons/Model.jsx
@@ -1,3 +1,3 @@
 export default function ModelIcon(props) {
-  return <img src="/assets/nfts.svg" className="h-4 w-4" {...props} />;
+  return <img src="/assets/users.svg" className="h-4 w-4" {...props} />;
 }
