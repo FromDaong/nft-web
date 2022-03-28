2019aa7aa3fae88398f08590cfb3eda93d3abc48
Kamfeskaya <chris@devtachom.com>
2022-03-28 15:17:11 +0200

    pull and merge

--- pages/creator-dashboard/index.js
+++ pages/creator-dashboard/index.js
@@ -1,33 +1,13 @@
-import {
-  ArrowRepeat,
-  CameraFill,
-  CollectionFill,
-  GearFill,
-  InfoCircleFill,
-  PatchCheckFill,
-  PencilFill,
-  PiggyBankFill,
-} from "react-bootstrap-icons";
 import { Button, Spinner } from "react-bootstrap";
-import { Nav, Tab } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
 import Axios from "axios";
 import BlankModal from "../../components/BlankModal";
 import CancelOrderModal from "../../components/CancelOrderModal";
-import CreatedNFTs from "../../components/CreatorDashboard/CreatedNFTs";
-import CreatorResources from "../../components/CreatorDashboard/CreatorResources";
-import EditProfile from "../../components/CreatorDashboard/EditProfile";
-import ErrorFallback from "../../components/Fallback/Error";
+import DashboardTabs from "../../components/CreatorDashboard/DashboardTabs";
 import Layout from "../../components/Layout";
 import Link from "next/link";
 import ListOrderModal from "../../components/ListOrderModal";
-import OwnedNFTs from "../../components/CreatorDashboard/OwnedNFTs";
-import Referrals from "../../components/CreatorDashboard/Referrals";
-import ResaleNFTs from "../../components/CreatorDashboard/ResaleNFTs";
-import SubSettingsBox from "../../components/CreatorDashboard/SubSettingsBox";
-import SubscriptionNFTs from "../../components/CreatorDashboard/SubscriptionNFTs";
-import SubscriptionSettings from "../../components/CreatorDashboard/SubscriptionSettings";
 import TransferNFTModal from "../../components/TransferNFTModal";
 import { useMoralis } from "react-moralis";
 import useSWR from "swr";
@@ -292,138 +272,25 @@ const ViewNFT = ({
             className="mt-4 mt-md-0"
           ></div>
         </div>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="edit-profile">
-          <div className="mt-2 row">
-            <div className="col-md-2 p-0">
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item className="white-tp-bg">
-                  <Nav.Link eventKey="edit-profile">
-                    <PencilFill className="mr-2 mb-1" />
-                    Edit Profile
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="owned-nfts">
-                    <CollectionFill className="mr-2 mb-1" />
-                    Owned NFTs
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="resale-nfts">
-                    <ArrowRepeat className="mr-2 mb-1" />
-                    Resale NFTs
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="created-nfts">
-                    <CameraFill className="mr-2 mb-1" />
-                    Sweet Shop NFTs
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="subscription-nfts">
-                    <PatchCheckFill className="mr-2 mb-1" />
-                    Subscription NFTs
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="subscription-settings">
-                    <GearFill className="mr-2 mb-1" />
-                    Subscription Settings
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="creator-resources">
-                    <InfoCircleFill className="mr-2 mb-1" />
-                    Creator Resources
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="white-tp-bg mt-2">
-                  <Nav.Link eventKey="referrals">
-                    <PiggyBankFill className="mr-2 mb-1" />
-                    Referrals
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </div>
-            <div className="col-md-10 pr-0 mt-4 mt-md-0 pl-0 pl-md-3">
-              <Tab.Content>
-                <Tab.Pane eventKey="edit-profile">
-                  <EditProfile modelData={modelData} />
-                </Tab.Pane>
-                <Tab.Pane eventKey="owned-nfts">
-                  {!nftError ? (
-                    <OwnedNFTs
-                      listOrderClick={listOrderClick}
-                      transferNFTClick={transferNFTClick}
-                      ownedNFTData={ownedNFTData}
-                      isLoading={isOwnedLoading}
-                      navigate={navigate}
-                      error={ownedNFTDataError}
-                    />
-                  ) : (
-                    <ErrorFallback custom="Failed to load Owned NFTs" />
-                  )}
-                </Tab.Pane>
-                <Tab.Pane eventKey="resale-nfts">
-                  {!nftError ? (
-                    <ResaleNFTs
-                      cancelOrderClick={cancelOrderClick}
-                      transferNFTClick={transferNFTClick}
-                      resaleNFTData={resaleNFTData}
-                      revealNFTs={resaleNFTData}
-                      isLoading={isOpenOrdersLoading}
-                      navigate={navigate}
-                      error={resaleNFTDataError}
-                    />
-                  ) : (
-                    <ErrorFallback custom="Failed to load Resale NFTs" />
-                  )}
-                </Tab.Pane>
-                <Tab.Pane eventKey="created-nfts">
-                  {!nftError ? (
-                    <CreatedNFTs
-                      hideNFTs={hideNFTs}
-                      transferNFTClick={transferNFTClick}
-                      nftData={nftData}
-                      modelData={modelData}
-                    />
-                  ) : (
-                    <ErrorFallback custom="Failed to load NFTs" />
-                  )}
-                </Tab.Pane>
-                <Tab.Pane eventKey="subscription-nfts">
-                  {!subNftError ? (
-                    <SubscriptionNFTs
-                      hideNFTs={hideNFTs}
-                      transferNFTClick={transferNFTClick}
-                      nftData={subNftData}
-                      modelData={modelData}
-                    />
-                  ) : (
-                    <ErrorFallback custom="Error loading sub NFTs" />
-                  )}
-                </Tab.Pane>
-                <Tab.Pane eventKey="subscription-settings">
-                  <SubscriptionSettings />
-                  <SubSettingsBox />
-                </Tab.Pane>
-                <Tab.Pane eventKey="creator-resources">
-                  <CreatorResources />
-                </Tab.Pane>
-                <Tab.Pane eventKey="referrals">
-                  <Referrals
-                    hideNFTs={hideNFTs}
-                    transferNFTClick={transferNFTClick}
-                    isLoading={isLoading}
-                    nftData={nftData}
-                    modelData={modelData}
-                  />
-                </Tab.Pane>
-              </Tab.Content>
-            </div>
-          </div>
-        </Tab.Container>
+        <DashboardTabs
+          modelData={modelData}
+          nftError={nftError}
+          listOrderClick={listOrderClick}
+          transferNFTClick={transferNFTClick}
+          ownedNFTData={ownedNFTData}
+          isOwnedLoading={isOwnedLoading}
+          navigate={navigate}
+          ownedNFTDataError={ownedNFTDataError}
+          cancelOrderClick={cancelOrderClick}
+          resaleNFTData={resaleNFTData}
+          isOpenOrdersLoading={isOpenOrdersLoading}
+          resaleNFTDataError={resaleNFTDataError}
+          hideNFTs={hideNFTs}
+          nftData={nftData}
+          subNftError={subNftError}
+          subNftData={subNftData}
+          isLoading={isLoading}
+        />
       </div>
     </Layout>
   );
