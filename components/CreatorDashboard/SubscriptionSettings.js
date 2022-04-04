850022735648ee0cf7e458236afa56cf9ee1a2f2
Kamfeskaya <chris@devtachom.com>
2022-04-04 22:51:13 +0200

    fix: fix other profile stuff

--- components/CreatorDashboard/SubscriptionSettings.js
+++ components/CreatorDashboard/SubscriptionSettings.js
@@ -10,17 +10,16 @@ import { useFormik } from "formik";
 import useGetSubscriptionCost from "../../hooks/useGetSubscriptionCost";
 import { useMoralis } from "react-moralis";
 import { useRouter } from "next/router";
-import useSWR from "swr";
 import { useState } from "react";
 
-const EditProfile = ({}) => {
+const EditProfile = ({ modelData }) => {
   const [showPendingModal, setShowPendingModal] = useState(null);
   const [showCompleteModal, setShowCompleteModal] = useState(null);
   const router = useRouter();
   const { account } = useMoralis();
   const subscriptionCost = useGetSubscriptionCost(account);
-  const { data: res } = useSWR(`/api/model/find-by-address/${account}`);
 
+  const res = modelData;
   const formik = useFormik({
     initialValues: {
       price:
