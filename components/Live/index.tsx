fdfff36c6cdca0a0df926923659fbf8740fb7c7f
Kamfeskaya <chris@devtachom.com>
2022-04-07 13:24:03 +0200

    feat: add interface for video

--- components/Live/index.tsx
+++ components/Live/index.tsx
@@ -1,30 +1,59 @@
 import {
   Button,
+  Flex,
+  Grid,
+  GridItem,
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
 
-export default function LiveStreamModal({ isOpen, onClose, data }) {
-  const {
-    playback_id: playbackId,
-    stream_key: streamKey,
-    stream_id: streamId,
-  } = data;
+import LiveVideo from "./Video";
+
+export default function LiveStreamModal({
+  isOpen,
+  onClose,
+  data,
+  streamIsActive,
+}) {
+  const { playback_id, stream_key, stream_id } = data;
+
+  const endStream = async () => {
+    await fetch(`/api/stream/${stream_id}/end`);
+    //mutate("/api/dashboard/stream");
+  };
+
   return (
     <>
       <Modal onClose={onClose} size={"full"} isOpen={isOpen}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody></ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>End Live</Button>
+          <ModalHeader>Livestream</ModalHeader>
+          <ModalBody h={"full"}>
+            <Flex flexDir="column" w={"full"}>
+              <Grid
+                gridTemplateColumns={"repeat(5, 1fr)"}
+                w={"full"}
+                h={"full"}
+              >
+                <GridItem colSpan={1} h={"full"}></GridItem>
+                <GridItem colSpan={3} h={"full"} bgColor="black">
+                  <LiveVideo
+                    streamIsActive={streamIsActive}
+                    playback_id={playback_id}
+                  />
+                </GridItem>
+                <GridItem colSpan={1} h={"full"}></GridItem>
+              </Grid>
+            </Flex>
+          </ModalBody>
+          <ModalFooter justifyContent={"flex-start"}>
+            <Button colorScheme={"red"} onClick={onClose}>
+              End Livestream
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
