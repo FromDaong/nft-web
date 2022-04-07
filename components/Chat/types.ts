b69419de8347bb04a7091c93a0d9b44e0650e62b
Kamfeskaya <chris@devtachom.com>
2022-04-07 11:17:15 +0200

    feat: add enable chat endpoint

--- components/Chat/types.ts
+++ components/Chat/types.ts
@@ -23,3 +23,22 @@ export interface ChatProps {
   };
   owner: string;
 }
+
+export const validateSHA256Address = (address: string): boolean => {
+  const regex = /^[0-9a-f]{64}$/;
+  return regex.test(address);
+};
+
+export const getParticipantByAddress = (
+  address: string,
+  arr: Array<ChatParticipant>
+): ChatParticipant => {
+  // validate this is actually a SHA256 address
+  if (!validateSHA256Address(address)) {
+    return null;
+  }
+  const participant = arr.find(
+    (participant: ChatParticipant) => participant.address === address
+  );
+  return participant;
+};
