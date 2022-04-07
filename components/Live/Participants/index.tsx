93cf55ae093fb1f75b5f97e22a670b7b070c0371
Kamfeskaya <chris@devtachom.com>
2022-04-07 14:01:32 +0200

    feat: stable streaming modal

---
+++ components/Live/Participants/index.tsx
@@ -0,0 +1,6 @@
+import { ChatProps } from "../types";
+import { Flex } from "@chakra-ui/react";
+
+export default function Participants(props: ChatProps) {
+  return <Flex className="w-full p-2"></Flex>;
+}
---
+++ components/Live/types.ts
@@ -0,0 +1,30 @@
+export type ChatType = "group-chat" | "one-to-one" | "broadcast" | "support";
+
+export interface ChatMessage {
+  text: string;
+  sender: string;
+  timestamp: number;
+  index: number;
+}
+
+export interface ChatParticipant {
+  username: string;
+  user_id: string;
+  _id: string;
+  address: string;
+}
+export interface ChatProps {
+  type: ChatType;
+  participants: Array<ChatParticipant>;
+  lastMessage: ChatMessage;
+  messages: {
+    total: number;
+    docs: Array<ChatMessage>;
+  };
+  owner: string;
+}
+
+export interface LiveVideoProps {
+  playback_id: string;
+  streamIsActive: boolean;
+}
