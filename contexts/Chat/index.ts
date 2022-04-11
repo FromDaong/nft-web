e6795b196a4094038d195c7dfed908777456b4a4
Kamfeskaya <chris@devtachom.com>
2022-04-11 23:22:09 +0200

    feat: add context structure

--- contexts/Chat/index.ts
+++ contexts/Chat/index.ts
@@ -1,11 +1,20 @@
+import { ChatMessage, ChatParticipant } from "../../components/Live/types";
 import { createContext, useEffect } from "react";
 
 export const LiveStreamChatContext = createContext<{
   currently_playing: string | null;
-  messages: Array<{}>;
+  messages: Array<ChatMessage>;
+  last_message: ChatMessage | null;
+  participants: Array<ChatParticipant>;
+  host: string | null;
+  isHost: boolean;
 }>({
   currently_playing: null,
   messages: [],
+  last_message: null,
+  participants: [],
+  host: null,
+  isHost: false,
 });
 
 export const LiveStreamChatContextProvider = ({ children }) => {
