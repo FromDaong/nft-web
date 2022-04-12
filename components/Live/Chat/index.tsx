f5a6ffddfb6e1be4c05798e828498c9f8aea3b9a
Kamfeskaya <chris@devtachom.com>
2022-04-12 12:32:38 +0200

    restyle chatbox

--- components/Live/Chat/index.tsx
+++ components/Live/Chat/index.tsx
@@ -3,7 +3,14 @@ import SendMessageBox from "./SendMessageBox";
 
 export default function ChatBox() {
   return (
-    <Flex className="w-full p-2" position="absolute" zIndex={500000} bottom={0}>
+    <Flex
+      w={["full", 3 / 4, "0.5", 1 / 3]}
+      className="p-2"
+      position="absolute"
+      zIndex={500000}
+      bottom={0}
+      left={0}
+    >
       <SendMessageBox />
     </Flex>
   );
