8e522e5b740093f32e52d8dc8a9f2fb045d5be48
Kamfeskaya <chris@devtachom.com>
2022-04-12 09:28:48 +0200

    feat: add chat routes

---
+++ pages/api/v2/chat/[chatId]/index.ts
@@ -0,0 +1,5 @@
+import { withJWTAuth } from "../../../../utils/server-utils";
+
+async function chatId(req, res) {}
+
+export default withJWTAuth(chatId);
---
+++ pages/api/v2/chat/index.ts
@@ -0,0 +1,5 @@
+import { withJWTAuth } from "../../../../utils/server-utils";
+
+async function chat(req, res) {}
+
+export default withJWTAuth(chat);
