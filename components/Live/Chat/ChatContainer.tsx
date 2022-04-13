98b604de17a4bbb8d03aec7e24046e7126b48332
Kamfeskaya <chris@devtachom.com>
2022-04-13 11:31:42 +0200

    chat items

--- components/Live/Chat/ChatContainer.tsx
+++ components/Live/Chat/ChatContainer.tsx
@@ -1,13 +1,21 @@
 import { Box } from "@chakra-ui/react";
 import ChatItem from "./ChatItem";
 import { Notification } from "../types";
-import { useEffect } from "react";
 
 export default function ChatContainer(props) {
   return (
-    <Box w={"full"} h={"20vh"} py={2} mb={2} position="relative">
+    <Box
+      w={"full"}
+      h={"20vh"}
+      py={2}
+      mb={2}
+      position="relative"
+      overflowY="hidden"
+      id="chat-container"
+    >
       {props.messages.map((m: Notification) => (
         <ChatItem
+          isLastMessage={m.index === props.messages[0]?.index}
           key={m.index}
           text={m.payload.text}
           user_id={m.payload.sender}
