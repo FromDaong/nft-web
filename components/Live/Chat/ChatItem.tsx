42ed0507128876bec105b0d8db961a2ba4f02bb9
Kamfeskaya <chris@devtachom.com>
2022-04-13 11:31:42 +0200

    chat items

--- components/Live/Chat/ChatItem.tsx
+++ components/Live/Chat/ChatItem.tsx
@@ -1,6 +1,22 @@
 import { Box, Text } from "@chakra-ui/react";
 
-export default function ChatItem({ text, user_id, timestamp, type, sent }) {
+import { useEffect } from "react";
+
+export default function ChatItem({
+  text,
+  user_id,
+  timestamp,
+  type,
+  sent,
+  isLastMessage,
+}) {
+  useEffect(() => {
+    // if is last message scroll to bottom
+    if (isLastMessage) {
+      const chatContainer = document.getElementById("chat-container");
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  });
   return (
     <Box py={1} opacity={sent ? 1 : 0.5}>
       <Text color={"white"} fontWeight={"semibold"}>
