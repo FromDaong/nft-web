b1a55cfede2862764a4b1730fbd3cfdd8691d884
Kamfeskaya <chris@devtachom.com>
2022-04-13 11:13:07 +0200

    feat: display chat bubbles

--- components/Live/Chat/ChatItem.tsx
+++ components/Live/Chat/ChatItem.tsx
@@ -1,5 +1,12 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
-export default function ChatItem() {
-  return <Box></Box>;
+export default function ChatItem({ text, user_id, timestamp, type, sent }) {
+  return (
+    <Box py={1} opacity={sent ? 1 : 0.5}>
+      <Text color={"white"} fontWeight={"semibold"}>
+        {user_id}
+      </Text>
+      <Text color="white">{text}</Text>
+    </Box>
+  );
 }
