b5bccd5e64f15f8c14fd49b0aab22c7b163b1c50
Kamfeskaya <chris@devtachom.com>
2022-04-14 19:33:25 +0200

    feat: add channel

---
+++ pages/api/v2/chat/[channel]/index.ts
@@ -0,0 +1,24 @@
+import NotificationModel from "../../../../../models/Notification";
+import { withJWTAuth } from "../../../../../utils/server-utils";
+
+async function chat(req, res) {
+  const { channel } = req.data;
+
+  try {
+    const notifications = await NotificationModel.find({
+      channel,
+    });
+
+    return res.status(200).json({
+      error: false,
+      notifications,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: true,
+    });
+  }
+}
+
+export default withJWTAuth(chat);
