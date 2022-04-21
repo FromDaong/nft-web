fb0cb088ecff59f14ebc1eac32d8d82a2df19cbf
Kamfeskaya <chris@devtachom.com>
2022-04-21 14:22:08 +0200

    fix: style button correctly

--- components/Live/Participants/index.tsx
+++ components/Live/Participants/index.tsx
@@ -3,7 +3,7 @@ import ParticipantsAvatarGroup from "./PartcipantsAvatarGroup";
 
 export default function Participants() {
   return (
-    <Flex className="w-full p-2" position="absolute" zIndex={500000} top={0}>
+    <Flex zIndex={500000}>
       <ParticipantsAvatarGroup
         participants={[
           { user_id: "Chris", avatar: "" },
