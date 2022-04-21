03c0e8e44a3e7c2e2d847d82db8a3f21055bfaad
Kamfeskaya <chris@devtachom.com>
2022-04-21 14:40:16 +0200

    feat: add context from participants

--- components/Live/Participants/index.tsx
+++ components/Live/Participants/index.tsx
@@ -1,19 +1,10 @@
 import { Flex } from "@chakra-ui/react";
 import ParticipantsAvatarGroup from "./PartcipantsAvatarGroup";
 
-export default function Participants() {
+export default function Participants({ participants }) {
   return (
     <Flex zIndex={500000}>
-      <ParticipantsAvatarGroup
-        participants={[
-          { user_id: "Chris", avatar: "" },
-          { user_id: "Chris", avatar: "" },
-          { user_id: "Chris", avatar: "" },
-          { user_id: "Chris", avatar: "" },
-          { user_id: "Chris", avatar: "" },
-          { user_id: "Chris", avatar: "" },
-        ]}
-      />
+      <ParticipantsAvatarGroup participants={participants} />
     </Flex>
   );
 }
