e41e14515b253478f1715a334a0cecdbae6a4379
Kamfeskaya <chris@devtachom.com>
2022-04-22 12:12:05 +0200

    feat: add reaction bubbles

--- components/Live/Chat/ChatUtilsBox.tsx
+++ components/Live/Chat/ChatUtilsBox.tsx
@@ -1,5 +1,10 @@
 import { Box } from "@chakra-ui/react";
+import ReactionBubble from "./ReactionBubble";
 
 export default function ChatUtilsBox() {
-  return <Box></Box>;
+  return (
+    <Box>
+      <ReactionBubble />
+    </Box>
+  );
 }
