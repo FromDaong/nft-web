2d3b5d2e74ee11bccbcbc15be432ab665e0806cb
Kamfeskaya <chris@devtachom.com>
2022-04-22 12:12:05 +0200

    feat: add reaction bubbles

--- components/Live/Chat/ReactionBubble.tsx
+++ components/Live/Chat/ReactionBubble.tsx
@@ -1,5 +1,17 @@
+import { AnimatedEmoji } from "react-native-animated-emoji";
 import { Box } from "@chakra-ui/react";
 
 export default function ReactionBubble() {
-  return <Box></Box>;
+  return (
+    <Box position="relative">
+      <AnimatedEmoji
+        index={"emoji.key"} // index to identity emoji component
+        style={{ bottom: 200 }} // start bottom position
+        name={"sweat_smile"} // emoji name
+        size={30} // font size
+        duration={4000} // ms
+        onAnimationCompleted={this.onAnimationCompleted} // completion handler
+      />
+    </Box>
+  );
 }
