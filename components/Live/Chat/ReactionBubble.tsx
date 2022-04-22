e41cfcff42f5b9d2189bb09bf644a0f23f45b182
Kamfeskaya <chris@devtachom.com>
2022-04-22 15:57:05 +0200

    feat: add reaction messages

--- components/Live/Chat/ReactionBubble.tsx
+++ components/Live/Chat/ReactionBubble.tsx
@@ -1,17 +1,11 @@
-import { AnimatedEmoji } from "react-native-animated-emoji";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
-export default function ReactionBubble() {
+export default function ReactionBubble({ sendEmoji }) {
   return (
-    <Box position="relative">
-      <AnimatedEmoji
-        index={"emoji.key"} // index to identity emoji component
-        style={{ bottom: 200 }} // start bottom position
-        name={"sweat_smile"} // emoji name
-        size={30} // font size
-        duration={4000} // ms
-        onAnimationCompleted={this.onAnimationCompleted} // completion handler
-      />
+    <Box position="relative" display="flex" experimental_spaceX={2}>
+      <Button onClick={() => sendEmoji("fire")}>🔥</Button>
+      <Button onClick={() => sendEmoji("squid")}>🦑</Button>
+      <Button onClick={() => sendEmoji("laugh")}>🤣</Button>
     </Box>
   );
 }
