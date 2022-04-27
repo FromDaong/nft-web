405d628d7920a0788011bb7a0f45c0ad0d2601a7
Kamfeskaya <chris@devtachom.com>
2022-04-27 23:23:30 +0200

    feat: add logger

---
+++ lib/logger.ts
@@ -0,0 +1,6 @@
+export default function logger(data) {
+  // Log name of function that called logger and the filename
+  console.log(
+    `${new Date().toLocaleString()} \n ${logger.caller.name} \n ${data}`
+  );
+}
