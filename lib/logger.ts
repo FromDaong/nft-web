f40f115367a59d3938650f7b549d839ee69c3a68
Kamfeskaya <chris@devtachom.com>
2022-04-28 01:15:01 +0200

    remove callee from l

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,6 +1,4 @@
 export default function logger(data) {
   // Log name of function that called logger and the filename
-  console.log(
-    `${new Date().toLocaleString()} \n ${logger.caller.name} \n ${data}`
-  );
+  console.log(`${new Date().toLocaleString()} \n \n ${data}`);
 }
