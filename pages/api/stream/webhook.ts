119ef3680e909035cd3a2a3e729e376432afbb06
Kamfeskaya <chris@devtachom.com>
2022-05-05 12:10:04 +0200

    feat: deploy hook

--- pages/api/stream/webhook.ts
+++ pages/api/stream/webhook.ts
@@ -1,12 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
+import Model from "../../../models/Model";
+
 // enable this webhook to set live
 
-const livestream_hook = (req: NextApiRequest, res: NextApiResponse) => {
+const livestream_hook = async (req: NextApiRequest, res: NextApiResponse) => {
   const { body } = req;
+  if (!body || !body.webhookId || !body.event || !body.stream) {
+    res.status(400).json({ success: false });
+    return;
+  }
 
-  console.log({ body });
+  const { event, stream } = body;
 
-  res.status(200).json({ error: false });
+  try {
+    const model = await Model.findOneAndUpdate(
+      {
+        "live.playback_id": stream.playback_id,
+      },
+      {
+        livestream_active: event === "stream.active" ? true : false,
+      },
+      { new: true }
+    );
+    console.log({ model });
+    res.status(200).json({ error: false });
+  } catch (err) {
+    console.log({ err });
+    res.status(400).json({ error: true, message: err });
+  }
 };
 
 export default livestream_hook;
