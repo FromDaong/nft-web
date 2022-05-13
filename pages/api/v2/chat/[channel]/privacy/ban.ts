dd49df15760ca4688aa43365f84f268e770c287d
Kamfeskaya <chris@devtachom.com>
2022-05-13 09:47:23 +0200

    trigger events from pusher

--- pages/api/v2/chat/[channel]/privacy/ban.ts
+++ pages/api/v2/chat/[channel]/privacy/ban.ts
@@ -1,10 +1,15 @@
 import Ban from "@models/privacy/Ban";
 import Model from "@models/Model";
+import { nodePusher } from "@lib/pusher";
 import { withJWTAuth } from "./../../../../../../utils/server-utils";
+
 const enforceIsHost = (handler) => async (req, res) => {
   const { ethAddress } = req.session;
+  const { channel } = req.query;
 
-  const host = await Model.findOne({});
+  const host = await Model.findOne({
+    "live.playback_id": channel,
+  });
   if (`${ethAddress}`.toUpperCase() === `${host.address}`.toUpperCase()) {
     handler(req, res);
   } else {
@@ -14,6 +19,12 @@ const enforceIsHost = (handler) => async (req, res) => {
 
 export async function ban(req, res) {
   const { channel } = req.query;
+  const channelName = `live-${channel}`;
+  const eventName = "ban-event";
+  const data = {
+    ...req.body,
+    host: req.session.ethAddress,
+  };
 
   try {
     const { toggle } = req.body;
@@ -26,13 +37,18 @@ export async function ban(req, res) {
       });
       await ban.save();
       res.status(200).json({ success: true });
-    } else if (toggle === "unban") {
+    } else if (toggle === "lift") {
       await Ban.deleteOne({ address: req.body.address, channel });
       res.status(200).json({ success: true });
     } else {
       const banned = await Ban.findOne({ channel });
       res.status(200).json({ banned });
     }
+
+    await nodePusher.trigger(channelName, eventName, {
+      ...data,
+    });
+
     return;
   } catch (err) {
     return res.status(500).send("Error banning user");
