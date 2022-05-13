82510b71a46bc67cb9feff7c87d07bac2912e8df
Kamfeskaya <chris@devtachom.com>
2022-05-13 09:25:53 +0200

    release bans

--- components/Live/Chat/ChatItem.tsx
+++ components/Live/Chat/ChatItem.tsx
@@ -10,9 +10,9 @@ import {
 } from "@chakra-ui/react";
 import { useCallback, useContext, useEffect } from "react";
 
-import { ChevronDownIcon } from "@chakra-ui/icons";
 import { LiveStreamChatContext } from "../../../contexts/Chat";
 import { ThreeDotsVertical } from "react-bootstrap-icons";
+import { useMoralis } from "react-moralis";
 
 export default function ChatItem({
   text,
@@ -22,6 +22,7 @@ export default function ChatItem({
   retry,
   index,
 }) {
+  const { account } = useMoralis();
   useEffect(() => {
     // if is last message scroll to bottom
     if (isLastMessage) {
@@ -30,8 +31,15 @@ export default function ChatItem({
     }
   });
 
-  const { retryMessage, messages, isBanned, banAddress, liftBan, kickout } =
-    useContext(LiveStreamChatContext);
+  const {
+    retryMessage,
+    messages,
+    isBanned,
+    banAddress,
+    liftBan,
+    kickout,
+    host,
+  } = useContext(LiveStreamChatContext);
   const retrySendMessage = useCallback(
     () => retryMessage(messages.find((i) => i.index === index)),
     [messages]
@@ -44,23 +52,25 @@ export default function ChatItem({
         <Text flex={1} fontWeight={"semibold"}>
           {`${user_id.substring(0, 6)}...${user_id.substr(-5)}`}
         </Text>
-        <Menu>
-          <>
-            <MenuButton as={Button} variant="link" size="sm">
-              <ThreeDotsVertical />
-            </MenuButton>
-            <MenuList>
-              <MenuItem
-                onClick={() => {
-                  isBanned ? liftBan(user_id) : kickout(user_id);
-                }}
-              >
-                Ban{" "}
-              </MenuItem>
-              <MenuItem onClick={() => kickout(user_id)}>Kick out</MenuItem>
-            </MenuList>
-          </>
-        </Menu>
+        {account === host && (
+          <Menu>
+            <>
+              <MenuButton as={Button} variant="link" size="sm">
+                <ThreeDotsVertical />
+              </MenuButton>
+              <MenuList>
+                <MenuItem
+                  onClick={() => {
+                    isBanned ? liftBan(user_id) : banAddress(user_id);
+                  }}
+                >
+                  Ban{" "}
+                </MenuItem>
+                <MenuItem onClick={() => kickout(user_id)}>Kick out</MenuItem>
+              </MenuList>
+            </>
+          </Menu>
+        )}
       </Flex>
       <Text color="gray.600">{text}</Text>
       {showRetryMessage && (
