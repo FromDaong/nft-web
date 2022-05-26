6861e12881d816a3f3ad972cbff8dad57b572a82
Luca <lucamarchal@gmail.com>
2022-05-26 18:16:36 +0100

    become creator bug

--- pages/become-creator.js
+++ pages/become-creator.js
@@ -151,7 +151,7 @@ const CreateModel = () => {
       if (res.rejected === false && res.pending === false) {
         return setStep("accepted");
       }
-      if (!res.identity_access_key) {
+      if (res.pending && !res.identity_access_key) {
         return setStep("verify");
       }
       if (res.pending && res?.identity_access_key?.length < 1) {
