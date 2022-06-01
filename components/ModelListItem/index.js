daef79695be2fa67b0aed68e208abba2c140e662
Kamfeskaya <chris@devtachom.com>
2022-06-01 02:00:09 -0700

    defer daoCdnUrl

--- components/ModelListItem/index.js
+++ components/ModelListItem/index.js
@@ -2,7 +2,7 @@ import { Button } from "@chakra-ui/react";
 import GumletImage from "../../components/Image/GumletImage";
 
 const ModelListItem = ({ data }) => {
-  const profilePic = data.profilePicCdnUrl ?? data.profile_pic;
+  const profilePic = data.profile_pic;
   const profilePicUrl = `/api/v2/utils/images/fetchWithFallback?default=${profilePic}`;
   return (
     <a href={`/creator/${data.username.replace("@", "")}`}>
