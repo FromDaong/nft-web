e7248f1fff6e5a2ab1a67a1c499e423017c8fd9e
Luca <lucamarchal@gmail.com>
2022-06-09 15:18:06 +0100

    new buy page

--- pages/buy.js
+++ pages/buy.js
@@ -9,11 +9,11 @@ const Totm = () => {
           titleClass="primary"
           subtitle="You can buy BNB easily through floorz.trade's widget below. Use BNB throughout the Treat site or purchase $TREAT."
         />
-        <div>
+        <div style={{ marginTop: -30 }}>
           <iframe
             width="100%"
             height="800px"
-            src="https://flooz.trade/wallet/0x01bd7acb6ff3b6dd5aefa05cf085f2104f3fc53f?refId=ikUONy"
+            src="https://flooz.trade/embedded/0x01bd7acb6fF3B6Dd5aefA05CF085F2104f3fC53F?backgroundColor=transparent&lightMode=true&refId=ikUONy"
           />
         </div>
       </center>
