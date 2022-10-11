ff1de11d8a2463bfd00d7d2aee294721539ed90b
Kamfeskaya <tatendachris@gmail.com>
2022-10-11 12:05:13 +0200

    feat: add trunk

--- core/contexts/Modals/Modals.tsx
+++ core/contexts/Modals/Modals.tsx
@@ -1,53 +1,59 @@
-import React, { createContext, useCallback, useState } from 'react'
-import styled from 'styled-components'
+import React, { createContext, useCallback, useState } from "react";
+import styled from "styled-components";
 
 interface ModalsContext {
-  content?: React.ReactNode,
-  isOpen?: boolean,
-  onPresent: (content: React.ReactNode, key?: string) => void,
-  onDismiss: () => void
+  content?: React.ReactNode;
+  isOpen?: boolean;
+  onPresent: (content: React.ReactNode, key?: string) => void;
+  onDismiss: () => void;
 }
 
 export const Context = createContext<ModalsContext>({
   onPresent: () => {},
   onDismiss: () => {},
-})
+});
 
 const Modals: React.FC = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [content, setContent] = useState<React.ReactNode>()
-  const [modalKey, setModalKey] = useState<string>()
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState<React.ReactNode>();
+  const [modalKey, setModalKey] = useState<string>();
 
-  const handlePresent = useCallback((modalContent: React.ReactNode, key?: string) => {
-    setModalKey(key)
-    setContent(modalContent)
-    setIsOpen(true)
-  }, [setContent, setIsOpen, setModalKey])
+  const handlePresent = useCallback(
+    (modalContent: React.ReactNode, key?: string) => {
+      setModalKey(key);
+      setContent(modalContent);
+      setIsOpen(true);
+    },
+    [setContent, setIsOpen, setModalKey]
+  );
 
   const handleDismiss = useCallback(() => {
-    setContent(undefined)
-    setIsOpen(false)
-  }, [setContent, setIsOpen, modalKey])
+    setContent(undefined);
+    setIsOpen(false);
+  }, [setContent, setIsOpen, modalKey]);
 
   return (
-    <Context.Provider value={{
-      content,
-      isOpen,
-      onPresent: handlePresent,
-      onDismiss: handleDismiss,
-    }}>
+    <Context.Provider
+      value={{
+        content,
+        isOpen,
+        onPresent: handlePresent,
+        onDismiss: handleDismiss,
+      }}
+    >
       {children}
       {isOpen && (
         <StyledModalWrapper>
           <StyledModalBackdrop onClick={handleDismiss} />
-          {React.isValidElement(content) && React.cloneElement(content, {
-            onDismiss: handleDismiss,
-          })}
+          {React.isValidElement(content) &&
+            React.cloneElement(content, {
+              onDismiss: handleDismiss,
+            })}
         </StyledModalWrapper>
       )}
     </Context.Provider>
-  )
-}
+  );
+};
 
 const StyledModalWrapper = styled.div`
   align-items: center;
@@ -55,13 +61,19 @@ const StyledModalWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   position: fixed;
-  top: 0; right: 0; bottom: 0; left: 0;
-`
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
 
 const StyledModalBackdrop = styled.div`
-  background-color: ${props => props.theme.color.grey[600]}aa;
+  background-color: ${(props) => props.theme.color.grey[600]}aa;
   position: absolute;
-  top: 0; right: 0; bottom: 0; left: 0;
-`
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
 
-export default Modals
+export default Modals;
--- core/contexts/Transactions/context.ts
+++ core/contexts/Transactions/context.ts
@@ -1,12 +1,12 @@
-import { createContext } from 'react'
-import { Transaction, TransactionsMap } from './types'
+import { createContext } from "react";
+import { Transaction, TransactionsMap } from "./types";
 
 interface TransactionsContext {
-  transactions: TransactionsMap
-  onAddTransaction: (tx: Transaction) => void
+  transactions: TransactionsMap;
+  onAddTransaction: (tx: Transaction) => void;
 }
 
 export default createContext<TransactionsContext>({
   transactions: {},
   onAddTransaction: (tx: Transaction) => {},
-})
+});
