aca6e501989f5c77af86a44672b0d141a702aa98
Kamfeskaya <tatendachris@gmail.com>
2022-10-16 16:15:17 +0200

    feat: add prisma schema

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/index.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
 import { Router } from "next/router";
 import Axios from "axios";
@@ -7,10 +8,9 @@ import ProgressBar from "@badrap/bar-of-progress";
 import { Container } from "packages/shared/components/Container";
 import Navbar from "packages/navigation/Navbar";
 import Footer from "packages/shared/components/Footer";
-import Commandbar from "packages/commandbar";
 import { useDisclosure } from "packages/hooks";
-import { useEffect } from "react";
 import { ThemeProvider } from "packages/theme";
+import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react";
 
 const progress = new ProgressBar({
   size: 3,
@@ -27,35 +27,37 @@ Axios.defaults.withCredentials = true;
 function MyApp({ Component, pageProps }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  useEffect(() => {
-    onOpen();
-  }, []);
-
   return (
     <ThemeProvider>
-      <Head>
-        <title>Treat DAO</title>
-        <meta name="title" content="Treat DAO" />
-        <meta name="image" content="https://i.imgur.com/OEiuwp4.jpg" />
-        <meta property="og:image" content="https://i.imgur.com/OEiuwp4.jpg" />
+      <FpjsProvider
+        loadOptions={{
+          apiKey: "5LG7UBlESl7pJPHsQjiI",
+          region: "eu",
+        }}
+      >
+        <Head>
+          <title>Treat DAO</title>
+          <meta name="title" content="Treat DAO" />
+          <meta name="image" content="https://i.imgur.com/OEiuwp4.jpg" />
+          <meta property="og:image" content="https://i.imgur.com/OEiuwp4.jpg" />
 
-        <meta
-          name="description"
-          content="Treat is an exclusive platform for creators to sell NFTs. Hold $TREAT to have a say on which creators are chosen & new platform features."
-        />
-      </Head>
-      <Commandbar isOpen={isOpen} onClose={onClose} />
-      <div className="py-3">
-        <Navbar />
-      </div>
-      <Container className="max-w-6xl mx-auto">
-        <main className="mt-8">
-          <Component {...pageProps} />
-        </main>
-      </Container>
-      <div className="py-12">
-        <Footer />
-      </div>
+          <meta
+            name="description"
+            content="Treat is an exclusive platform for creators to sell NFTs. Hold $TREAT to have a say on which creators are chosen & new platform features."
+          />
+        </Head>
+        <div className="py-3">
+          <Navbar />
+        </div>
+        <Container className="max-w-6xl mx-auto">
+          <main className="mt-8">
+            <Component {...pageProps} />
+          </main>
+        </Container>
+        <div className="py-12">
+          <Footer />
+        </div>
+      </FpjsProvider>
     </ThemeProvider>
   );
 }
