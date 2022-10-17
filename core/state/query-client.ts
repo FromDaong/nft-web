896055d6b392d395f205236ff83990cda7f5c8e2
Kamfeskaya <tatendachris@gmail.com>
2022-10-17 12:08:25 +0200

    feat: add backend structure

---
+++ core/state/query-client.ts
@@ -0,0 +1,5 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const qc = new QueryClient();
+
+export default qc;
---
+++ packages/seo/page/SEOHead.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function SEOHead(seo_props: object) {
+  return (
+    <Head>
+      <></>
+    </Head>
+  );
+}
