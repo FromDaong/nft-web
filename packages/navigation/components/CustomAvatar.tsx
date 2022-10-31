ee22b743ec2373832e6ee8d28ef3879eb5a23939
Kamfeskaya <tatendachris@gmail.com>
2022-11-01 01:06:24 +0200

    feat: implement rainbow connect

---
+++ packages/navigation/components/CustomAvatar.tsx
@@ -0,0 +1,41 @@
+import { AvatarComponent } from "@rainbow-me/rainbowkit";
+
+const generateColorFromAddress = (address: string) => {
+  const hash = stringToHash(address);
+  const hue = hash % 360;
+  return `hsl(${hue}, 100%, 50%)`;
+};
+
+const stringToHash = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+};
+
+
+const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
+  const color = generateColorFromAddress(address);
+  return ensImage ? (
+    <img
+      src={ensImage}
+      width={size}
+      height={size}
+      style={{ borderRadius: 999 }}
+    />
+  ) : (
+    <div
+      style={{
+        backgroundColor: color,
+        borderRadius: 999,
+        height: size,
+        width: size,
+      }}
+    >
+      :^)
+    </div>
+  );
+};
+
+export default CustomAvatar;
