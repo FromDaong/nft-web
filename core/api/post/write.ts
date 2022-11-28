8f5b71b40643e4e69583f28f40199d22616e549d
Kamfeskaya <chris@devtachom.com>
2022-11-28 05:39:45 +0200

    feat: add post writes

---
+++ core/api/post/write.ts
@@ -0,0 +1,50 @@
+import { NextApiResponse } from "next";
+import { NextApiRequest } from "next";
+import { Like } from "./types";
+
+export const like_post = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.body.data)
+    return res.status(500).json({
+      error: true,
+      message: "Could not complete that action. Please try again.",
+    });
+
+  const data: Like = req.body.data;
+  // Persist like to database
+
+  return res.status(200).json({ error: false, message: "Ok" });
+};
+
+export const unlike_post = (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.body.data)
+    return res.status(500).json({
+      error: true,
+      message: "Could not complete that action. Please try again.",
+    });
+
+  const data: Like = req.body.data;
+  // Persist like to database
+
+  return res.status(200).json({ error: false, message: "Ok" });
+};
+
+export const create_post = (req: NextApiRequest, res: NextApiResponse) => {};
+
+export const archive_post = (req: NextApiRequest, res: NextApiResponse) => {};
+
+export const tip_post = (req: NextApiRequest, res: NextApiResponse) => {};
+
+export const save_to_collection = (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {};
+
+export const send_offer_for_creator = (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {};
+
+export const get_post_shorturl = (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {};
