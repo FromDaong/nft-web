0c2ff0078b04c35b8c4157f94dea76d2632ebe5b
codesleyah <93673396+codesleyah@users.noreply.github.com>
2022-12-01 14:50:55 +0200

    feat: complete landing page sections

--- packages/shared/components/Card/MarketingPages/SmartContractsCard.tsx
+++ packages/shared/components/Card/MarketingPages/SmartContractsCard.tsx
@@ -1,25 +1,38 @@
-import { CardDetailSection, HeadlessCard } from "..";
-import { Button } from "../../Button";
-import { Container } from "../../Container";
-import { ContextualHeading, Text } from "../../Typography/Headings";
+import Link from "next/link";
+import {HeadlessCard} from "..";
+import {Button} from "../../Button";
+import {Container} from "../../Container";
+import {ContextualHeading, Text} from "../../Typography/Headings";
 
 export default function SmartContracts() {
-  return (
-    <Container>
-      <div style={{ maxWidth: "60%", display: "block" }}>
-        <CardDetailSection>
-          <ContextualHeading>
-            Verified, open-source projects from the TreatDAO team
-          </ContextualHeading>
-          <Text>
-            TreatDAO is a community owned project built to life easier for
-            content creators. All the contracts used on TreatDAO projects are
-            open source and verified on supported blockchains.
-          </Text>
-        </CardDetailSection>
+	return (
+		<HeadlessCard appearance={"gradient"}>
+			<Container
+				className="flex flex-col gap-8"
+				style={{maxWidth: "60%"}}
+			>
+				<Container className="flex flex-col gap-4">
+					<ContextualHeading>
+						Verified, open-source projects from the TreatDAO team
+					</ContextualHeading>
+					<Text>
+						TreatDAO is a community owned project built to life easier for
+						content creators. All the contracts used on TreatDAO projects are
+						open source and verified on supported blockchains.
+					</Text>
+				</Container>
 
-        <Button>Contribute on GitHub</Button>
-      </div>
-    </Container>
-  );
+				<Container>
+					<Link href={"https://github.com/TreatDAODev"}>
+						<a
+							target={"_blank"}
+							rel={"noopener"}
+						>
+							<Button css={{padding: "12px 24px"}}>Contribute on GitHub</Button>
+						</a>
+					</Link>
+				</Container>
+			</Container>
+		</HeadlessCard>
+	);
 }
--- packages/shared/components/Card/MarketingPages/SubscriptionsCard.tsx
+++ packages/shared/components/Card/MarketingPages/SubscriptionsCard.tsx
@@ -1,40 +1,40 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
-import { CardDetailSection, HeadlessCard } from "..";
-import { Button } from "../../Button";
-import { ContextualHeading, Text } from "../../Typography/Headings";
-import { Overlay } from "./BenefitsCard";
+import {ArrowRightIcon} from "@heroicons/react/solid";
+import {CardDetailSection, HeadlessCard} from "..";
+import {Button} from "../../Button";
+import {Container} from "../../Container";
+import {ContextualHeading, Text} from "../../Typography/Headings";
 
 export default function SubscriptionsCard() {
-  return (
-    <HeadlessCard
-      css={{
-        background: "url('/assets/backgrounds/fans_card_bg.jpeg'), top",
-      }}
-    >
-      <Overlay />
-      <div style={{ maxWidth: "60%", display: "block", zIndex: 1 }}>
-        <CardDetailSection>
-          <ContextualHeading>
-            Recurring on-chain payments, ever heard of them?
-          </ContextualHeading>
-          <Text>
-            TreatDAO is a community owned project built to life easier for
-            content creators. All the contracts used on TreatDAO projects are
-            open source and verified on supported blockchains.
-          </Text>
-        </CardDetailSection>
+	return (
+		<HeadlessCard className="shadow-lg">
+			<Container
+				className="flex flex-col gap-8"
+				style={{maxWidth: "60%", zIndex: 1}}
+			>
+				<Container className="flex flex-col gap-4">
+					<ContextualHeading>
+						Recurring on-chain payments, ever heard of them?
+					</ContextualHeading>
+					<Text>
+						TreatDAO is a community owned project built to life easier for
+						content creators. All the contracts used on TreatDAO projects are
+						open source and verified on supported blockchains.
+					</Text>
+				</Container>
 
-        <Button appearance={"default"}>
-          Find out how we do it{" "}
-          <ArrowRightIcon
-            style={{
-              height: "1.1rem",
-              width: "1.1rem",
-              marginLeft: "0.8rem",
-            }}
-          />
-        </Button>
-      </div>
-    </HeadlessCard>
-  );
+				<Container>
+					<Button css={{padding: "12px 24px"}}>
+						Find out how we do it{" "}
+						<ArrowRightIcon
+							style={{
+								height: "1.1rem",
+								width: "1.1rem",
+								marginLeft: "0.8rem",
+							}}
+						/>
+					</Button>
+				</Container>
+			</Container>
+		</HeadlessCard>
+	);
 }
