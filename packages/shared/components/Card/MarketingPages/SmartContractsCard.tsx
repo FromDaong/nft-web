55b132e1a260529a4541e375806f4354aafdb8b1
codesleyah <93673396+codesleyah@users.noreply.github.com>
2022-12-02 00:15:09 +0200

    feat: import landing page content

--- packages/shared/components/Card/MarketingPages/SmartContractsCard.tsx
+++ packages/shared/components/Card/MarketingPages/SmartContractsCard.tsx
@@ -1,25 +1,25 @@
-import { CardDetailSection, HeadlessCard } from "..";
-import { Button } from "../../Button";
-import { Container } from "../../Container";
-import { ContextualHeading, Text } from "../../Typography/Headings";
+import {CardDetailSection, HeadlessCard} from "..";
+import {Button} from "../../Button";
+import {Container} from "../../Container";
+import {ContextualHeading, Text} from "../../Typography/Headings";
 
 export default function SmartContracts() {
-  return (
-    <Container>
-      <div style={{ maxWidth: "60%", display: "block" }}>
-        <CardDetailSection>
-          <ContextualHeading>
-            Verified, open-source projects from the TreatDAO team
-          </ContextualHeading>
-          <Text>
-            TreatDAO is a community owned project built to life easier for
-            content creators. All the contracts used on TreatDAO projects are
-            open source and verified on supported blockchains.
-          </Text>
-        </CardDetailSection>
+	return (
+		<Container>
+			<div style={{maxWidth: "60%", display: "block"}}>
+				<CardDetailSection>
+					<ContextualHeading>
+						Verified, open-source projects from the TreatDAO team
+					</ContextualHeading>
+					<Text>
+						TreatDAO is a community owned project built to life easier for
+						content creators. All the contracts used on TreatDAO projects are
+						open source and verified on supported blockchains.
+					</Text>
+				</CardDetailSection>
 
-        <Button>Contribute on GitHub</Button>
-      </div>
-    </Container>
-  );
+				<Button>Contribute on GitHub</Button>
+			</div>
+		</Container>
+	);
 }
