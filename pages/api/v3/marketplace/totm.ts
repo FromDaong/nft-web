65ad5d9b075934223b709d8f40f44c5c0faa8fa2
Kamfeskaya <tatendachris@gmail.com>
2022-12-05 23:00:29 +0200

    feat: add redis cache

--- pages/api/v3/marketplace/totm.ts
+++ pages/api/v3/marketplace/totm.ts
@@ -1,14 +1,22 @@
 import {returnWithError} from "./../../../../server/database/engine/utils";
 import {NextApiResponse} from "next";
 import {NextApiRequest} from "next";
-import {connectMongoDB} from "server/database/engine";
+import {
+	connectMongoDB,
+	getStringFromRedisCache,
+	setStringToRedisCache,
+} from "server/database/engine";
 import {returnWithSuccess} from "server/database/engine/utils";
 import LegacyCreatorModel from "server/database/legacy/creator/Creator";
 import LegacyNFTModel from "server/database/legacy/nft/NFT";
 
 export default async function totm(req: NextApiRequest, res: NextApiResponse) {
-	await connectMongoDB();
+	const cached_totm = await getStringFromRedisCache("totm");
+	if (cached_totm) {
+		return returnWithSuccess(cached_totm, res);
+	}
 
+	await connectMongoDB();
 	const totm_model = await LegacyCreatorModel.findOne({totm: true});
 
 	if (!totm_model) {
@@ -18,6 +26,8 @@ export default async function totm(req: NextApiRequest, res: NextApiResponse) {
 	const totm_nfts = await LegacyNFTModel.find({
 		model_handle: totm_model.username,
 	});
+
+	await setStringToRedisCache("totm", totm_nfts);
 
 	return returnWithSuccess(totm_nfts, res);
 }
