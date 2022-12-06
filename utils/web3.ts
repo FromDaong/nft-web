a9eeb00c2a50d531882b20124347c60e868a662c
Kamfeskaya <tatendachris@gmail.com>
2022-12-07 01:27:12 +0200

    feat: add rpc urls

--- utils/web3.ts
+++ utils/web3.ts
@@ -1,7 +1,6 @@
 import Web3 from "web3";
 
-const url =
-  "https://nd-421-513-967.p2pify.com/4b6934a4a6a6ace1d6ba8644eae82d6e";
+const url = process.env.NEXT_PUBLIC_RPC_NODE_URL;
 const web3 = new Web3(url);
 
 export default web3;
