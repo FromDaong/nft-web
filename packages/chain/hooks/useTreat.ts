cc8471a049fe99482055e8cdf5e2a316cfe378b6
codesleyah <93673396+codesleyah@users.noreply.github.com>
2022-12-08 16:29:36 +0200

    feat: add treat provider

--- packages/chain/hooks/useTreat.ts
+++ packages/chain/hooks/useTreat.ts
@@ -1,9 +1,9 @@
-import { useContext } from 'react'
-import { Context } from '../contexts/TreatProvider'
+import {Context} from "@contexts/TreatProvider";
+import {useContext} from "react";
 
 const useTreat = () => {
-  const { treat } = useContext(Context)
-  return treat
-}
+	const {treat} = useContext(Context);
+	return treat;
+};
 
-export default useTreat
+export default useTreat;
