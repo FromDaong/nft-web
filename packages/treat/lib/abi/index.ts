241841fa77dcb39ef458f794478e0ecc9f9c1b00
codesleyah <93673396+codesleyah@users.noreply.github.com>
2022-12-08 16:56:34 +0200

    feat: add abi file

--- packages/treat/lib/abi/index.ts
+++ packages/treat/lib/abi/index.ts
@@ -1,5 +1,43 @@
-import TreatNFTMinterABI from "packages/treat/lib/abi/treatnftminter.json";
+import TreatNFTMinterABI from "./treatnftminter.json";
+import CreatorMartAbi from "./creatormart.json";
+import GenericBep20 from "./erc20.json";
+import MasterMelonFarmerAbi from "./mastermelonfarmer.json";
+import MelonAbi from "./melontoken.json";
+import MelonMartAbi from "./melonmart.json";
+import MinterPermissionHelperAbi from "./nftminterpermissionhelper.json";
+import PancakeLPAbi from "./treatpancakelp.json";
+import SubscriberMartAbi from "./subscribermart.json";
+import TippingContractAbi from "./tippingcontract.json";
+import TotwMinterHelperAbi from "./totwminterhelper.json";
+import Treat2Abi from "./treat2.json";
+import TreatMarketReaderAbi from "./treatmarketreader.json";
+import TreatMarketplaceAbi from "./treatMarketplace.json";
+import TreatMartAbi from "./treatmart.json";
+import TreatSubscriptionsAbi from "./treatsubscriptions.json";
+import TreatTradeInAbi from "./treattradein.json";
+import TreatV1ForV2Abi from "./treatv1forv2.json";
+import WETHAbi from "./weth.json";
+import TreatNFTMinterV1Abi from "./treatnftminterv1.json";
+import TreatAbi from "./treat.json";
 
 export const ABI = {
 	treatMinter: TreatNFTMinterABI,
+	creatorMart: CreatorMartAbi,
+	generic: GenericBep20,
+	masterMelonFarmer: MasterMelonFarmerAbi,
+	melon: MelonAbi,
+	melonMart: MelonMartAbi,
+	minterPermissionHelper: MinterPermissionHelperAbi,
+	pancakeLP: PancakeLPAbi,
+	subscriberMart: SubscriberMartAbi,
+	tipping: TippingContractAbi,
+	totmMinter: TotwMinterHelperAbi,
+	treat: Treat2Abi,
+	treatMarketReader: TreatMarketReaderAbi,
+	treatMarketplace: TreatMarketplaceAbi,
+	treatMart: TreatMartAbi,
+	treatSubscribtions: TreatSubscriptionsAbi,
+	treatTradeIn: TreatTradeInAbi,
+	treatV1ForV2: TreatV1ForV2Abi,
+	weth: WETHAbi,
 };
