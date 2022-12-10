6694e7d9550101b752df135bead6c8f96f63d429
codesleyah <93673396+codesleyah@users.noreply.github.com>
2022-12-10 18:56:37 +0200

    fix: introduce nextIron session

--- core/chain/connect.tsx
+++ core/chain/connect.tsx
@@ -4,6 +4,9 @@ import {
 	connectorsForWallets,
 	DisclaimerComponent,
 	darkTheme,
+	RainbowKitAuthenticationProvider,
+	createAuthenticationAdapter,
+	AuthenticationStatus,
 } from "@rainbow-me/rainbowkit";
 import {
 	injectedWallet,
@@ -15,12 +18,14 @@ import {
 import {Chain, configureChains, createClient, WagmiConfig} from "wagmi";
 import {alchemyProvider} from "wagmi/providers/alchemy";
 import {publicProvider} from "wagmi/providers/public";
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useMemo, useRef, useState} from "react";
 import {
 	RainbowKitSiweNextAuthProvider,
 	GetSiweMessageOptions,
 } from "core/auth/components/AuthenticationProvider";
 import {useTheme} from "@packages/theme";
+import authenticationAdapter from "./connectAdapter";
+import {SiweMessage} from "siwe";
 
 const binance: Chain = {
 	id: 56,
@@ -87,6 +92,92 @@ const Disclaimer: DisclaimerComponent = ({Text, Link}) => (
 const WagmiWrapper = ({children}: {children: ReactNode}) => {
 	const {theme} = useTheme();
 
+	const fetchingStatusRef = useRef(false);
+	const verifyingRef = useRef(false);
+	const [authStatus, setAuthStatus] = useState<AuthenticationStatus>("loading");
+
+	// Fetch user when:
+	useEffect(() => {
+		const fetchStatus = async () => {
+			if (fetchingStatusRef.current || verifyingRef.current) {
+				return;
+			}
+
+			fetchingStatusRef.current = true;
+
+			try {
+				const response = await fetch("/api/me");
+				const json = await response.json();
+				setAuthStatus(json.address ? "authenticated" : "unauthenticated");
+			} catch (_error) {
+				setAuthStatus("unauthenticated");
+			} finally {
+				fetchingStatusRef.current = false;
+			}
+		};
+
+		// 1. page loads
+		fetchStatus();
+
+		// 2. window is focused (in case user logs out of another window)
+		window.addEventListener("focus", fetchStatus);
+		return () => window.removeEventListener("focus", fetchStatus);
+	}, []);
+
+	const authAdapter = useMemo(() => {
+		return createAuthenticationAdapter({
+			getNonce: async () => {
+				const response = await fetch("/api/nonce");
+				return await response.text();
+			},
+
+			createMessage: ({nonce, address, chainId}) => {
+				return new SiweMessage({
+					domain: window.location.host,
+					address,
+					statement: "Sign in with Ethereum to the app.",
+					uri: window.location.origin,
+					version: "1",
+					chainId,
+					nonce,
+				});
+			},
+
+			getMessageBody: ({message}) => {
+				return message.prepareMessage();
+			},
+
+			verify: async ({message, signature}) => {
+				verifyingRef.current = true;
+
+				try {
+					const response = await fetch("/api/verify", {
+						method: "POST",
+						headers: {"Content-Type": "application/json"},
+						body: JSON.stringify({message, signature}),
+					});
+
+					const authenticated = Boolean(response.ok);
+
+					if (authenticated) {
+						setAuthStatus(authenticated ? "authenticated" : "unauthenticated");
+					}
+
+					return authenticated;
+				} catch (error) {
+					return false;
+				} finally {
+					verifyingRef.current = false;
+				}
+			},
+
+			signOut: async () => {
+				setAuthStatus("unauthenticated");
+				await fetch("/api/logout");
+			},
+		});
+	}, []);
+
 	const themes = {
 		light: lightTheme({
 			accentColor: "#121212",
@@ -101,8 +192,9 @@ const WagmiWrapper = ({children}: {children: ReactNode}) => {
 
 	return (
 		<WagmiConfig client={wagmiClient}>
-			<RainbowKitSiweNextAuthProvider
-				getSiweMessageOptions={getSiweMessageOptions}
+			<RainbowKitAuthenticationProvider
+				adapter={authAdapter}
+				status={authStatus}
 			>
 				<RainbowKitProvider
 					modalSize="compact"
@@ -116,7 +208,7 @@ const WagmiWrapper = ({children}: {children: ReactNode}) => {
 				>
 					{children}
 				</RainbowKitProvider>
-			</RainbowKitSiweNextAuthProvider>
+			</RainbowKitAuthenticationProvider>
 		</WagmiConfig>
 	);
 };
