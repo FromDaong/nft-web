0b37cbedcfc96d4c1ee3583cdb84343488ca3a7d
Kamfeskaya <tatendachris@gmail.com>
2023-06-07 14:41:12 +0200

    fix: add card styles

--- components/NFTCard/cards/PortfolioListingCard.tsx
+++ components/NFTCard/cards/PortfolioListingCard.tsx
@@ -2,6 +2,9 @@ import {Container} from "@packages/shared/components/Container";
 import {useTritNFTUtils} from "@packages/post/hooks";
 import {TritPostProps} from "@packages/post/types";
 import {NFTCard} from "..";
+import {Heading} from "@packages/shared/components/Typography/Headings";
+import {Button} from "@packages/shared/components/Button";
+import {EnterFullScreenIcon} from "@radix-ui/react-icons";
 
 export default function PortfolioPublicListingCard(props: TritPostProps) {
 	const {liked, likeNFT, isMine, isProtected} = useTritNFTUtils(props);
@@ -10,9 +13,9 @@ export default function PortfolioPublicListingCard(props: TritPostProps) {
 	const editions = new Array(+props.count).fill(0).map((_, i) => i + 1);
 	return (
 		<NFTCard _id={props._id}>
-			<Container className="relative shadow">
+			<Container className="relative">
 				<Container
-					className="relative"
+					className="relative shadow cursor-pointer rounded-xl overflow-clip group"
 					css={{zIndex: 1}}
 				>
 					<NFTCard.RenderMedia
@@ -21,11 +24,34 @@ export default function PortfolioPublicListingCard(props: TritPostProps) {
 						text={props.text}
 						_id={props._id}
 					/>
+					<Container
+						className="absolute top-0 left-0 flex-col items-center justify-end hidden w-full h-full gap-4 p-4 pb-8 transition-opacity duration-300 group-hover:flex"
+						css={{
+							zIndex: 10,
+							background:
+								"linear-gradient(180deg, rgba(0, 0, 0, 0.04) 0%, rgba(0, 0, 0, 0.525208) 57.08%, rgba(0, 0, 0, 0.85) 100%)",
+						}}
+					>
+						<Heading
+							css={{color: "$white"}}
+							size={"xs"}
+						>
+							{props.name}
+						</Heading>
+						<Button
+							className="w-fit"
+							appearance={"surface"}
+							css={{borderRadius: 999}}
+						>
+							<EnterFullScreenIcon className="w-4 h-4" />
+							View in fullscreen
+						</Button>
+					</Container>
 				</Container>
 				{editions.slice(0, 3).map((_, i) => (
 					<Container
 						key={i}
-						className="absolute rounded-xl top-0 right-0 border shadow"
+						className="absolute top-0 right-0 border shadow rounded-xl"
 						css={{
 							width: `${100 - (i + 1) * 10}%`,
 							height: "100%",
@@ -38,27 +64,6 @@ export default function PortfolioPublicListingCard(props: TritPostProps) {
 						}}
 					/>
 				))}
-			</Container>
-
-			<Container
-				className={`flex flex-col w-full gap-2 mt-4`}
-				css={{
-					marginTop: "1rem !important",
-				}}
-			>
-				<NFTCard.Detail
-					{...props}
-					isMine={isMine}
-					liked={liked}
-					likeNFT={likeNFT}
-					unlikeNFT={likeNFT}
-					creator={props.creator.username}
-					toggleImageProtection={() => null}
-					isProtected={isProtected}
-					hideSeller={props.hideSeller}
-					count={props.count}
-					soldOut={soldOut}
-				/>
 			</Container>
 		</NFTCard>
 	);
