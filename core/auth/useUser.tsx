53a093b8d4ae204c67174ff5dac2731d3815ae59
Kamfeskaya <tatendachris@gmail.com>
2023-06-08 01:36:00 +0200

    fix: build error

---
+++ core/auth/useUser.tsx
@@ -0,0 +1,26 @@
+import {useSession} from "next-auth/react";
+import {useEffect, useState} from "react";
+
+export const useUser = () => {
+	const {data, status} = useSession();
+	const [isConnected, setConnected] = useState(false);
+
+	const profile = (data as any)?.profile;
+	const creator = (data as any)?.creator;
+	const isLoading = status === "loading";
+
+	useEffect(() => {
+		if (status === "authenticated") {
+			setConnected(true);
+		} else {
+			setConnected(false);
+		}
+	}, [status]);
+
+	return {
+		isConnected,
+		isLoading,
+		profile,
+		creator,
+	};
+};
---
+++ pages/api/v3/marketplace/listings/resale/seller/[seller_address].ts
@@ -0,0 +1,80 @@
+import request, {gql} from "graphql-request";
+import {NextApiRequest, NextApiResponse} from "next";
+import {returnWithSuccess} from "server/helpers/core/utils";
+import {MongoModelNFT, MongoModelProfile} from "server/helpers/models";
+import Web3 from "web3";
+
+const RESALE_GRAPHQL_ENDPOINT =
+	"https://api.thegraph.com/subgraphs/name/treatdaodev/treatdao";
+
+const query = gql`
+    query listings(address: String!, skip: Int!, first: Int!, sort: String!) {
+        marketItems(
+				orderBy: $sort
+				orderDirection: desc
+				skip: $skip
+				first: 12
+                where: { seller: $address }
+			) {
+				nft
+				currentSupply
+				cost
+				isActive
+				transactionHash
+				seller
+			}
+    }
+`;
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const {seller_address, page, sort} = req.query;
+	const {marketItems} = await request(RESALE_GRAPHQL_ENDPOINT, query, {
+		sort: (sort as string) ?? "cost",
+		// sort: "id" as "totalSales" | "totalSupply" | "id",
+		skip: (parseInt(page as string) - 1) * 24,
+		first: 24,
+		address: (seller_address as string).toLowerCase(),
+	});
+
+	const ids = marketItems.map((item) => item.nft);
+	const nfts = await MongoModelNFT.find({
+		id: {
+			$in: ids,
+		},
+	});
+
+	const populatedNfts = await Promise.all(
+		nfts.map(async (nft) => {
+			const order = marketItems.find((o) => parseInt(o.nft) === nft.id);
+			const thisSeller = await MongoModelProfile.findOne({
+				address: (seller_address as string).toLowerCase(),
+			}).exec();
+
+			const nftprice = Web3.utils.fromWei(order.cost);
+			const nftSeller = {
+				...thisSeller?.toObject(),
+				address: seller_address,
+			};
+			return {
+				...nft.toObject(),
+				price: nftprice,
+				seller: nftSeller,
+			};
+		})
+	);
+
+	return returnWithSuccess(
+		{
+			docs: populatedNfts.flat(),
+			totalDocs: 10000,
+			hasNextPage: true,
+			hasPrevPage: +page > 1,
+			page: +page,
+			nextPage: +page + 1,
+		},
+		res
+	);
+}
