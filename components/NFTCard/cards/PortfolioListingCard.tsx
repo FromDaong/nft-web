7ad9f05b03ed228291ac6923bf87e946e6efe380
Kamfeskaya <tatendachris@gmail.com>
2023-06-07 21:34:18 +0200

    fix: add styling to card

--- components/NFTCard/cards/PortfolioListingCard.tsx
+++ components/NFTCard/cards/PortfolioListingCard.tsx
@@ -6,13 +6,14 @@ import {Heading, Text} from "@packages/shared/components/Typography/Headings";
 import {Button} from "@packages/shared/components/Button";
 import {EnterFullScreenIcon} from "@radix-ui/react-icons";
 import {ImportantText} from "@packages/shared/components/Typography/Text";
+import {ArrowRight, ShoppingBag} from "lucide-react";
 
 export default function PortfolioPublicListingCard(props: TritPostProps) {
 	const {liked, likeNFT, isMine, isProtected} = useTritNFTUtils(props);
 
 	const soldOut = props.collection?.minted === props.max_supply;
 	const editions = new Array(+props.count).fill(0).map((_, i) => i + 1);
-	console.log(props.count);
+	console.log(props);
 	return (
 		<NFTCard _id={props._id}>
 			<Container className="relative">
@@ -56,20 +57,40 @@ export default function PortfolioPublicListingCard(props: TritPostProps) {
 								"linear-gradient(180deg, rgba(0, 0, 0, 0.04) 0%, rgba(0, 0, 0, 0.525208) 57.08%, rgba(0, 0, 0, 0.85) 100%)",
 						}}
 					>
-						<Heading
-							css={{color: "$white"}}
-							size={"xs"}
-						>
-							{props.name}
-						</Heading>
-						<Button
-							className="w-fit"
-							appearance={"surface"}
-							css={{borderRadius: 999}}
-						>
-							<EnterFullScreenIcon className="w-4 h-4" />
-							View in fullscreen
-						</Button>
+						<Container className="flex flex-col items-center gap-2">
+							<NFTCard.Creator
+								avatar={props.creator.avatar}
+								username={props.creator.username}
+							/>
+							<Heading
+								css={{color: "$white"}}
+								size={"xs"}
+							>
+								{props.name}
+							</Heading>
+						</Container>
+						<Text css={{color: "$sand2"}}>{props.text}</Text>
+						{isMine && (
+							<Button
+								className="w-fit"
+								appearance={"surface"}
+								css={{borderRadius: 999}}
+							>
+								<EnterFullScreenIcon className="w-4 h-4" />
+								View in fullscreen
+							</Button>
+						)}
+						{!isMine && (
+							<Button
+								className="w-fit"
+								appearance={"surface"}
+								css={{borderRadius: 999}}
+							>
+								<ShoppingBag className="w-4 h-4" />
+								Buy on sweetshop
+								<ArrowRight className="w-4 h-4" />
+							</Button>
+						)}
 					</Container>
 				</Container>
 				{editions.slice(0, 3).map((_, i) => (
